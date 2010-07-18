23283b3854384de27c3f05f079b02a228df51fc3
Volker Krause <vkrause@kde.org>
2010-07-18 18:19:09 +0200

    First attempt on a full scene tree model.

--- scenemodel.cpp
+++ scenemodel.cpp
@@ -19,23 +19,26 @@ void SceneModel::setScene(QGraphicsScene* scene)
 
 QVariant SceneModel::data(const QModelIndex& index, int role) const
 {
-  QGraphicsItem *item = 0;
   if ( !index.isValid() )
     return QVariant();
-  if ( !index.parent().isValid() && index.row() >= 0 && index.row() < m_scene->items().size() )
-    item = m_scene->items().at( index.row() );
+  QGraphicsItem *item = static_cast<QGraphicsItem*>( index.internalPointer() );
 
   if ( item && role == Qt::DisplayRole ) {
     if ( index.column() == 0 )
       return QLatin1String( "0x" ) + QString::number( reinterpret_cast<qlonglong>( item ), 16 );
-    else if ( index.column() == 1 )
+    else if ( index.column() == 1 ) {
+      QGraphicsObject *obj = item->toGraphicsObject();
+      if ( obj )
+	return obj->metaObject()->className();
       return QString::number( item->type() );
+    }
   }
   return QVariant();
 }
 
 int SceneModel::columnCount(const QModelIndex& parent) const
 {
+  Q_UNUSED( parent );
   return 2;
 }
 
@@ -57,12 +60,21 @@ QModelIndex SceneModel::parent(const QModelIndex& child) const
 {
   if ( !child.isValid() )
     return QModelIndex();
-  return QModelIndex();
+  QGraphicsItem* item = static_cast<QGraphicsItem*>( child.internalPointer() );
+  if ( !item->parentItem() )
+    return QModelIndex();
+  int row = item->parentItem()->childItems().indexOf( item );
+  return createIndex( row, 0, item->parentItem() );
 }
 
 QModelIndex SceneModel::index(int row, int column, const QModelIndex& parent) const
 {
-  return createIndex( row, column, 0 );
+  if ( !parent.isValid() && row >= 0 && row < m_scene->items().size() )
+    return createIndex( row, column, m_scene->items().at( row ) );
+  QGraphicsItem* parentItem = static_cast<QGraphicsItem*>( parent.internalPointer() );
+  if ( !parentItem || parent.row() < 0 || parent.row() >= parentItem->childItems().size() )
+    return QModelIndex();
+  return createIndex( row, column, parentItem->childItems().at( row ) );
 }
 
 #include "scenemodel.moc"
