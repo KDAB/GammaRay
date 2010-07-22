19f11dea0e329539bbabc0b18f7277469841ff15
Volker Krause <vkrause@kde.org>
2010-07-22 09:27:24 +0200

    Add column headers and tooltips to the object property model.

--- objectpropertymodel.cpp
+++ objectpropertymodel.cpp
@@ -1,5 +1,6 @@
 #include "objectpropertymodel.h"
 #include <QtCore/QMetaProperty>
+#include <KLocalizedString>
 
 using namespace Endoscope;
 
@@ -15,19 +16,37 @@ void ObjectPropertyModel::setObject(QObject* object)
   reset();
 }
 
+static QString translateBool( bool value )
+{
+  return value ? i18n( "yes" ) : i18n( "no" );
+}
+
 QVariant ObjectPropertyModel::data(const QModelIndex& index, int role) const
 {
   if ( !index.isValid() || !m_obj || index.row() < 0 || index.row() >= m_obj->metaObject()->propertyCount() )
     return QVariant();
 
+  const QMetaProperty prop = m_obj->metaObject()->property( index.row() );
   if ( role == Qt::DisplayRole || role == Qt::EditRole ) {
-    const QMetaProperty prop = m_obj->metaObject()->property( index.row() );
     if ( index.column() == 0 )
       return prop.name();
     else if ( index.column() == 1 )
       return prop.read( m_obj );
     else if ( index.column() == 2 )
       return prop.typeName();
+  } else if ( role == Qt::ToolTipRole ) {
+    const QString toolTip = i18n( "Constant: %1\nDesignable: %2\nFinal: %3\nResetable: %4\n"
+      "Has notification: %5\nScriptable: %6\nStored: %7\nUser: %8\nWritable: %9",
+      translateBool( prop.isConstant() ),
+      translateBool( prop.isDesignable( m_obj ) ),
+      translateBool( prop.isFinal() ),
+      translateBool( prop.isResettable() ),
+      translateBool( prop.hasNotifySignal() ),
+      translateBool( prop.isScriptable( m_obj ) ),
+      translateBool( prop.isStored( m_obj ) ),
+      translateBool( prop.isUser( m_obj ) ),
+      translateBool( prop.isWritable() ) );
+    return toolTip;
   }
 
   return QVariant();
@@ -42,6 +61,18 @@ bool ObjectPropertyModel::setData(const QModelIndex& index, const QVariant& valu
     return prop.write( m_obj, value );
   }
   return QAbstractItemModel::setData(index, value, role);
+}
+
+QVariant ObjectPropertyModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+  if ( orientation == Qt::Horizontal && role == Qt::DisplayRole ) {
+    switch ( section ) {
+      case 0: return i18n( "Property" );
+      case 1: return i18n( "Value" );
+      case 2: return i18n( "Type" );
+    }
+  }
+  return QAbstractItemModel::headerData(section, orientation, role);
 }
 
 Qt::ItemFlags ObjectPropertyModel::flags(const QModelIndex& index) const
--- objectpropertymodel.h
+++ objectpropertymodel.h
@@ -15,6 +15,7 @@ class ObjectPropertyModel : public QAbstractTableModel
 
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
     Qt::ItemFlags flags(const QModelIndex& index) const;
     int columnCount(const QModelIndex& parent = QModelIndex()) const;
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
