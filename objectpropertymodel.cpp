4366a03b433377d26d91a82d0bca4b6a9ff4dcfb
Volker Krause <vkrause@kde.org>
2010-07-22 09:06:48 +0200

    Add object property editing.

--- objectpropertymodel.cpp
+++ objectpropertymodel.cpp
@@ -20,7 +20,7 @@ QVariant ObjectPropertyModel::data(const QModelIndex& index, int role) const
   if ( !index.isValid() || !m_obj || index.row() < 0 || index.row() >= m_obj->metaObject()->propertyCount() )
     return QVariant();
 
-  if ( role == Qt::DisplayRole ) {
+  if ( role == Qt::DisplayRole || role == Qt::EditRole ) {
     const QMetaProperty prop = m_obj->metaObject()->property( index.row() );
     if ( index.column() == 0 )
       return prop.name();
@@ -31,6 +31,33 @@ QVariant ObjectPropertyModel::data(const QModelIndex& index, int role) const
   }
 
   return QVariant();
+}
+
+bool ObjectPropertyModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+  if ( index.isValid() && m_obj && index.column() == 1 && index.row() >= 0
+    && index.row() < m_obj->metaObject()->propertyCount() && role == Qt::EditRole )
+  {
+    const QMetaProperty prop = m_obj->metaObject()->property( index.row() );
+    return prop.write( m_obj, value );
+  }
+  return QAbstractItemModel::setData(index, value, role);
+}
+
+Qt::ItemFlags ObjectPropertyModel::flags(const QModelIndex& index) const
+{
+  const Qt::ItemFlags flags = QAbstractItemModel::flags(index);
+
+  if ( !index.isValid() || !m_obj || index.column() != 1 || index.row() < 0
+    || index.row() >= m_obj->metaObject()->propertyCount() )
+  {
+    return flags;
+  }
+
+  const QMetaProperty prop = m_obj->metaObject()->property( index.row() );
+  if ( prop.isWritable() )
+    return flags | Qt::ItemIsEditable;
+  return flags;
 }
 
 int ObjectPropertyModel::columnCount(const QModelIndex& parent) const
--- objectpropertymodel.h
+++ objectpropertymodel.h
@@ -12,7 +12,10 @@ class ObjectPropertyModel : public QAbstractTableModel
   public:
     ObjectPropertyModel(QObject* parent = 0);
     void setObject( QObject *object );
+
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+    bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+    Qt::ItemFlags flags(const QModelIndex& index) const;
     int columnCount(const QModelIndex& parent = QModelIndex()) const;
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
 
