723391a1d3d9c41539b537be9d56f72537358d0d
Volker Krause <vkrause@kde.org>
2010-08-06 13:40:01 +0200

    build on systems with sizeof(int) < sizeof(void*)

--- util.cpp
+++ util.cpp
@@ -8,7 +8,7 @@ QString Util::displayString(const QObject* object)
 {
   if ( object->objectName().isEmpty() ) {
     return QString::fromLatin1( "0x%1 (%2)" )
-      .arg( QString::number( reinterpret_cast<int>( object ), 16 ) )
+      .arg( QString::number( reinterpret_cast<qulonglong>( object ), 16 ) )
       .arg( object->metaObject()->className() );
   }
   return object->objectName();
