c4a8d3e7ad3b7fcd865aab737d593dc84f36a152
Volker Krause <vkrause@kde.org>
2010-10-25 20:00:30 +0200

    Remove KStandardDirs dependency.
    LD_PRELOAD doesn't need an absolute path, so this just works without it.
    Also, don't eat the debug output of the probed process.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
 
-
-#include <KDE/KStandardDirs>
-
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
 #include <QtCore/QProcess>
@@ -9,10 +6,6 @@
 int main( int argc, char** argv )
 {
   QCoreApplication app( argc, argv );
-  const QString probe = KStandardDirs::locate( "lib", "libendoscope_probe.so" );
-  //qDebug() << probe;
-  if ( probe.isEmpty() )
-    qFatal( "Endoscope probe not found." );
   QStringList args = app.arguments();
   args.takeFirst(); // that's us
   if ( args.isEmpty() ) {
@@ -21,9 +14,10 @@ int main( int argc, char** argv )
   }
 
   QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-  env.insert( "LD_PRELOAD", probe );
+  env.insert( "LD_PRELOAD", "libendoscope_probe.so" );
   QProcess proc;
   proc.setProcessEnvironment( env );
+  proc.setProcessChannelMode( QProcess::ForwardedChannels );
   const QString program = args.takeFirst();
   proc.start( program, args );
   proc.waitForFinished( -1 );
