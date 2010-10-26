ad6519de65bfbec12181bdf595d1d74eb9b0a298
Allen Winter <allen@kdab.com>
2010-10-26 18:28:33 -0400

    if the LD_PRELOAD variable is already set, then use it. else, search in the typical places for libendoscope_probe.so, exiting nicely if it still cannot be found.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 
-#include <QtCore/QCoreApplication>
-#include <QtCore/QDebug>
-#include <QtCore/QProcess>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QProcess>
 
 int main( int argc, char** argv )
 {
@@ -14,7 +16,22 @@ int main( int argc, char** argv )
   }
 
   QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-  env.insert( "LD_PRELOAD", "libendoscope_probe.so" );
+  if ( env.value( "LD_PRELOAD").isEmpty() ) {
+    QStringList pldirs;
+    pldirs << "/usr/local/lib64" << "/usr/local/lib"
+           << "/opt/lib64" << "/opt/lib"
+           << "/usr/lib64" << "/usr/lib";
+    QDir::setSearchPaths( "preloads", pldirs );
+    QFile plfile( "preloads:libendoscope_probe.so");
+    if ( !plfile.exists() ) {
+      qWarning( "Cannot locate libendoscope_probe.so in the typical places.\n"
+                "Try setting the $LD_PRELOAD environment variable to the fullpath,\n"
+                "For example:"
+                "  export LD_PRELOAD=/opt/lib64/libendoscope_probe.so");
+      return 1;
+    }
+    env.insert( "LD_PRELOAD", plfile.fileName() );
+  }
   QProcess proc;
   proc.setProcessEnvironment( env );
   proc.setProcessChannelMode( QProcess::ForwardedChannels );
