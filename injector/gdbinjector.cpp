b13ecee9cf070b5e06f1438e72956d0e2ca42d56
Volker Krause <vkrause@kde.org>
2011-07-24 12:44:30 +0200

    Implement launching with the GDB injector as well.

--- injector/gdbinjector.cpp
+++ injector/gdbinjector.cpp
@@ -8,26 +8,55 @@
 
 using namespace Endoscope;
 
+int GdbInjector::launch(const QStringList& programAndArgs, const QString& probeDll, const QString& probeFunc)
+{
+  QStringList gdbArgs;
+  gdbArgs.push_back( QLatin1String("--args") );
+  gdbArgs.append( programAndArgs );
+
+  if ( !startGdb( gdbArgs ) )
+    return -1;
+
+  m_process->write( "break main\n" );
+  m_process->write( "run\n" );
+  m_process->write( "sha QtCore\n" );
+  m_process->write( "break QCoreApplication::exec\n" );
+  m_process->write( "continue\n" );
+
+  return injectAndDetach( probeDll, probeFunc );
+}
+
 bool GdbInjector::attach(int pid, const QString& probeDll, const QString& probeFunc)
 {
   Q_ASSERT( pid > 0 );
-  QProcess proc;
-  proc.setProcessChannelMode( QProcess::ForwardedChannels );
-  proc.start( QLatin1String("gdb"), QStringList() << QLatin1String("-pid") << QString::number(pid) );
-  if ( !proc.waitForStarted( -1 ) ) {
-    qWarning() << proc.errorString();
+  if ( !startGdb( QStringList() << QLatin1String("-pid") << QString::number(pid) ) )
     return -1;
-  }
-
-  proc.write( "sha dl\n" );
-  proc.write( qPrintable( QString::fromLatin1("call dlopen(\"%1\", %2)\n").arg( probeDll ).arg( RTLD_NOW ) ) );
-  proc.write( qPrintable( QString::fromLatin1("sha %1\n").arg( probeDll ) ) );
-  proc.write( qPrintable( QString::fromLatin1("call %1()\n").arg( probeFunc ) ) );
-  proc.write( "detach\n" );
-  proc.write( "quit\n" );
-  proc.waitForBytesWritten( -1 );
-
-  proc.waitForFinished( -1 );
-  return proc.exitCode();
+  return injectAndDetach( probeDll, probeFunc );
 }
 
+bool GdbInjector::startGdb(const QStringList& args)
+{
+  m_process.reset( new QProcess );
+//   m_process->setProcessChannelMode( QProcess::ForwardedChannels );
+  m_process->start( QLatin1String("gdb"), args );
+  if ( !m_process->waitForStarted( -1 ) ) {
+    qWarning() << m_process->errorString();
+    return false;
+  }
+  return true;
+}
+
+int GdbInjector::injectAndDetach(const QString& probeDll, const QString& probeFunc)
+{
+  Q_ASSERT( m_process );
+  m_process->write( "sha dl\n" );
+  m_process->write( qPrintable( QString::fromLatin1("call dlopen(\"%1\", %2)\n").arg( probeDll ).arg( RTLD_NOW ) ) );
+  m_process->write( qPrintable( QString::fromLatin1("sha %1\n").arg( probeDll ) ) );
+  m_process->write( qPrintable( QString::fromLatin1("call %1()\n").arg( probeFunc ) ) );
+  m_process->write( "detach\n" );
+  m_process->write( "quit\n" );
+  m_process->waitForBytesWritten( -1 );
+
+  m_process->waitForFinished( -1 );
+  return m_process->exitCode();
+}
--- injector/gdbinjector.h
+++ injector/gdbinjector.h
@@ -2,14 +2,22 @@
 #define ENDOSCOPE_GDBINJECTOR_H
 
 #include <injector/abstractinjector.h>
-
+#include <qprocess.h>
 
 namespace Endoscope {
 
 class GdbInjector : public AbstractInjector
 {
   public:
+    virtual int launch(const QStringList& programAndArgs, const QString& probeDll, const QString& probeFunc);
     virtual bool attach(int pid, const QString& probeDll, const QString& probeFunc);
+
+  private:
+    bool startGdb( const QStringList &args );
+    int injectAndDetach( const QString &probeDll, const QString &probeFunc );
+
+  private:
+    QScopedPointer<QProcess> m_process;
 };
 
 }
