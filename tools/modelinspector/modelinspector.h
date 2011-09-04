3dd19c63547383293e2326903e0b9713797327d0
Volker Krause <vkrause@kde.org>
2011-09-04 14:59:36 +0200

    Add missing license headers.

--- tools/modelinspector/modelinspector.h
+++ tools/modelinspector/modelinspector.h
@@ -1,3 +1,26 @@
+/*
+  modelinspector.h
+
+  This file is part of Endoscope, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2010-2011 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krauss@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ENDOSCOPE_MODELINSPECTOR_H
 #define ENDOSCOPE_MODELINSPECTOR_H
 
