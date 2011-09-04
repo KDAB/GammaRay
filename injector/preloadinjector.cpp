059d8959abcdeea793a8eb5f7207709981a8f467
Allen Winter <allen.winter@kdab.com>
2011-09-03 20:33:56 -0400

    Q_UNUSED() coding style

--- injector/preloadinjector.cpp
+++ injector/preloadinjector.cpp
@@ -29,22 +29,25 @@
 
 using namespace Endoscope;
 
-int PreloadInjector::launch(const QStringList& programAndArgs, const QString& probeDll, const QString& probeFunc)
+int PreloadInjector::launch(const QStringList &programAndArgs,
+                            const QString &probeDll,
+                            const QString &probeFunc)
 {
+  Q_UNUSED(probeFunc);
   QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
 #ifdef Q_OS_MAC
-  env.insert( "DYLD_INSERT_LIBRARIES", probeDll );
+  env.insert("DYLD_INSERT_LIBRARIES", probeDll);
 #else
-  env.insert( "LD_PRELOAD", probeDll );
+  env.insert("LD_PRELOAD", probeDll);
 #endif
   QProcess proc;
-  proc.setProcessEnvironment( env );
-  proc.setProcessChannelMode( QProcess::ForwardedChannels );
+  proc.setProcessEnvironment(env);
+  proc.setProcessChannelMode(QProcess::ForwardedChannels);
 
   QStringList args = programAndArgs;
   const QString program = args.takeFirst();
-  proc.start( program, args );
-  proc.waitForFinished( -1 );
+  proc.start(program, args);
+  proc.waitForFinished(-1);
 
   return proc.exitCode();
 }
