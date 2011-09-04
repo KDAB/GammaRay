5862fc06a67128913e9bc591e5213067e46c4d08
Stephen Kelly <stephen.kelly@kdab.com>
2011-09-04 19:26:37 +0200

    Add authorship for my contributions.

--- probe.cpp
+++ probe.cpp
@@ -6,6 +6,7 @@
 
   Copyright (C) 2010-2011 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
   Author: Volker Krause <volker.krause@kdab.com>
+  Author: Stephen Kelly <stephen.kelly@kdab.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
