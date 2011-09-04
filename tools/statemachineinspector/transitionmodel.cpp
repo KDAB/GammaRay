a27d3448a304145ab7897b23a9977b84d42a5806
Allen Winter <allen.winter@kdab.com>
2011-09-04 11:39:16 -0400

    Q_UNUSED() comment unused Q_D(const TransitionModel) in TransitionModel::parent() coding style

--- tools/statemachineinspector/transitionmodel.cpp
+++ tools/statemachineinspector/transitionmodel.cpp
@@ -21,7 +21,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include "transitionmodel.h"
 
 #include <QtCore/QStringList>
@@ -43,44 +42,46 @@ class TransitionModelPrivate
   TransitionModelPrivate(TransitionModel *qq)
     : q_ptr(qq), m_state(0)
   {
-
   }
 
   Q_DECLARE_PUBLIC(TransitionModel)
   TransitionModel * const q_ptr;
   QState * m_state;
 
-  QList<QObject*> children( QObject* parent ) const;
+  QList<QObject*> children(QObject *parent) const;
 
-  QObject* mapModelIndex2QObject( const QModelIndex& ) const;
+  QObject *mapModelIndex2QObject(const QModelIndex &) const;
 };
 
 }
 
-QList<QObject*> TransitionModelPrivate::children( QObject* parent ) const
+QList<QObject*> TransitionModelPrivate::children(QObject *parent) const
 {
-    QList<QObject*> result;
-    if (parent == 0)
-      parent = m_state;
+  QList<QObject*> result;
+  if (parent == 0) {
+    parent = m_state;
+  }
 
-    foreach(QObject *o, parent->children())
-      if (o->inherits("QAbstractTransition"))
-        result.append(o);
-
-    qSort( result );
-    return result;
-}
-
-QObject* TransitionModelPrivate::mapModelIndex2QObject( const QModelIndex& index ) const
-{
-    if ( index.isValid() ) {
-        QObjectList c = children( reinterpret_cast<QObject*>( index.internalPointer() ) );
-        return c[index.row()];
+  foreach (QObject *o, parent->children()) {
+    if (o->inherits("QAbstractTransition")) {
+      result.append(o);
     }
-    return m_state;
+  }
+
+  qSort(result);
+  return result;
 }
 
-TransitionModel::TransitionModel(QObject* parent)
+QObject *TransitionModelPrivate::mapModelIndex2QObject(const QModelIndex &index) const
+{
+  if (index.isValid()) {
+    QObjectList c = children(reinterpret_cast<QObject*>(index.internalPointer()));
+    return c[index.row()];
+  }
+  return m_state;
+}
+
+TransitionModel::TransitionModel(QObject *parent)
   : ObjectModelBase<QAbstractItemModel>(parent), d_ptr(new TransitionModelPrivate(this))
 {
   QHash<int, QByteArray> _roleNames = roleNames();
@@ -88,7 +89,7 @@ TransitionModel::TransitionModel(QObject* parent)
   setRoleNames(_roleNames);
 }
 
-void TransitionModel::setState(QState* state)
+void TransitionModel::setState(QState *state)
 {
   Q_D(TransitionModel);
   beginResetModel();
@@ -98,79 +99,90 @@ void TransitionModel::setState(QState* state)
 
 QVariant TransitionModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-  if (section < 2)
+  if (section < 2) {
     return ObjectModelBase<QAbstractItemModel>::headerData(section, orientation, role);
-  if (section == 2)
+  }
+  if (section == 2) {
     return "Signal";
-  if (section == 3)
+  }
+  if (section == 3) {
     return "Target";
+  }
   return QVariant();
 }
 
-QVariant TransitionModel::data( const QModelIndex& index, int role ) const
+QVariant TransitionModel::data(const QModelIndex &index, int role) const
 {
-    Q_D(const TransitionModel);
-    if ( !index.isValid() || !d->m_state )
-        return QVariant();
-
-    QObject* obj = d->mapModelIndex2QObject(index);
-
-    if ( obj ) {
-      if (index.column() == 2 && role == Qt::DisplayRole) {
-        QSignalTransition *sigTransition = qobject_cast<QSignalTransition*>(obj);
-        if (sigTransition)
-          return sigTransition->signal();
-      } else if (index.column() == 3 && role == Qt::DisplayRole) {
-        QAbstractTransition *transition = qobject_cast<QAbstractTransition*>(obj);
-        if (transition)
-          return Util::displayString(transition->targetState());
-      }
-      return dataForObject( obj, index, role );
-    }
+  Q_D(const TransitionModel);
+  if (!index.isValid() || !d->m_state) {
     return QVariant();
+  }
+
+  QObject *obj = d->mapModelIndex2QObject(index);
+
+  if (obj) {
+    if (index.column() == 2 && role == Qt::DisplayRole) {
+      QSignalTransition *sigTransition = qobject_cast<QSignalTransition*>(obj);
+      if (sigTransition) {
+        return sigTransition->signal();
+      }
+    } else if (index.column() == 3 && role == Qt::DisplayRole) {
+      QAbstractTransition *transition = qobject_cast<QAbstractTransition*>(obj);
+      if (transition) {
+        return Util::displayString(transition->targetState());
+      }
+    }
+    return dataForObject(obj, index, role);
+  }
+  return QVariant();
 }
 
-int TransitionModel::columnCount( const QModelIndex& parent ) const
+int TransitionModel::columnCount(const QModelIndex &parent) const
 {
   return ObjectModelBase<QAbstractItemModel>::columnCount(parent) + 2;
 }
 
-int TransitionModel::rowCount( const QModelIndex& parent ) const
+int TransitionModel::rowCount(const QModelIndex &parent) const
 {
-    Q_D(const TransitionModel);
-    if (!d->m_state)
-      return 0;
-    return d->children(d->mapModelIndex2QObject(parent)).count();
+  Q_D(const TransitionModel);
+  if (!d->m_state) {
+    return 0;
+  }
+  return d->children(d->mapModelIndex2QObject(parent)).count();
 }
 
-QModelIndex TransitionModel::index( int row, int column, const QModelIndex & parent ) const
+QModelIndex TransitionModel::index(int row, int column, const QModelIndex &parent) const
 {
-    Q_D(const TransitionModel);
+  Q_D(const TransitionModel);
 
-    if (parent.isValid() || !d->m_state)
-      return QModelIndex();
-
-    if (row < 0 || column < 0 || column > 3)
-      return QModelIndex();
-
-    QObject *internalPointer = reinterpret_cast<QObject*>(parent.internalPointer());
-    if ( !parent.isValid() ) {
-          internalPointer = d->m_state;
-    } else {
-          QObject *o = reinterpret_cast<QObject*>( parent.internalPointer() ) ;
-          QObjectList c = d->children( o );
-          internalPointer = c.at(parent.row());
-    }
-
-    QObjectList c = d->children(internalPointer);
-    if (row >= c.size())
-      return QModelIndex();
-
-    return createIndex( row, column, internalPointer );
-}
-
-QModelIndex TransitionModel::parent( const QModelIndex & index ) const
-{
-    Q_D(const TransitionModel);
+  if (parent.isValid() || !d->m_state) {
     return QModelIndex();
+  }
+
+  if (row < 0 || column < 0 || column > 3) {
+    return QModelIndex();
+  }
+
+  QObject *internalPointer = reinterpret_cast<QObject*>(parent.internalPointer());
+  if (!parent.isValid()) {
+    internalPointer = d->m_state;
+  } else {
+    QObject *o = reinterpret_cast<QObject*>(parent.internalPointer()) ;
+    QObjectList c = d->children(o);
+    internalPointer = c.at(parent.row());
+  }
+
+  QObjectList c = d->children(internalPointer);
+  if (row >= c.size()) {
+    return QModelIndex();
+  }
+
+  return createIndex(row, column, internalPointer);
+}
+
+QModelIndex TransitionModel::parent(const QModelIndex &index) const
+{
+  Q_UNUSED(index);
+  //Q_D(const TransitionModel);
+  return QModelIndex();
 }
--- tools/statemachineinspector/transitionmodel.h
+++ tools/statemachineinspector/transitionmodel.h
@@ -21,7 +21,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #ifndef TRANSITIONMODEL_H
 #define TRANSITIONMODEL_H
 
@@ -36,19 +35,19 @@ class TransitionModelPrivate;
 
 class TransitionModel : public ObjectModelBase<QAbstractItemModel>
 {
-public:
+  public:
     TransitionModel(QObject *parent = 0);
     void setState(QState *state);
-    int columnCount( const QModelIndex& parent = QModelIndex() ) const;
-    int rowCount ( const QModelIndex& parent = QModelIndex() ) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    int rowCount (const QModelIndex &parent = QModelIndex()) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-    QVariant data ( const QModelIndex& index, int role = Qt::DisplayRole ) const;
-    QModelIndex index ( int row, int column, const QModelIndex& parent = QModelIndex() ) const;
-    QModelIndex parent ( const QModelIndex & index ) const;
+    QVariant data (const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &index) const;
 
-protected:
+  protected:
     Q_DECLARE_PRIVATE(TransitionModel)
-    TransitionModelPrivate * const d_ptr;
+    TransitionModelPrivate *const d_ptr;
 };
 
 }
