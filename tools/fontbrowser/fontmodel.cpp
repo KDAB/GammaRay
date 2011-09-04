b715f6791a4393f0486a417ba911e2ea0e68f8d2
Allen Winter <allen.winter@kdab.com>
2011-09-04 11:49:28 -0400

    Q_UNUSED() style cleanup

--- tools/fontbrowser/fontmodel.cpp
+++ tools/fontbrowser/fontmodel.cpp
@@ -21,15 +21,13 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include "fontmodel.h"
 
 using namespace Endoscope;
 
-FontModel::FontModel(QObject* parent)
+FontModel::FontModel(QObject *parent)
   : QAbstractItemModel(parent)
 {
-
 }
 
 QList<QFont> FontModel::currentFonts() const
@@ -44,7 +42,7 @@ void FontModel::updateFonts(const QList<QFont> &fonts)
   endResetModel();
 }
 
-void FontModel::updateText(const QString& text)
+void FontModel::updateText(const QString &text)
 {
   beginResetModel();
   m_text = text;
@@ -54,87 +52,106 @@ void FontModel::updateText(const QString& text)
 QVariant FontModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
   if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
-    if (section == 0)
+    if (section == 0) {
       return "Text";
-    if (section == 1)
+    }
+    if (section == 1) {
       return "Font";
-    if (section == 2)
+    }
+    if (section == 2) {
       return "Point size";
-    if (section == 3)
+    }
+    if (section == 3) {
       return "Bold";
-    if (section == 4)
+    }
+    if (section == 4) {
       return "Italic";
-    if (section == 5)
+    }
+    if (section == 5) {
       return "Underline";
+    }
   }
   return QAbstractItemModel::headerData(section, orientation, role);
 }
 
-int FontModel::rowCount(const QModelIndex& parent) const
+int FontModel::rowCount(const QModelIndex &parent) const
 {
-  if (parent.isValid())
+  if (parent.isValid()) {
     return 0;
+  }
   return m_fonts.size();
 }
 
-QModelIndex FontModel::index(int row, int column, const QModelIndex& parent) const
+QModelIndex FontModel::index(int row, int column, const QModelIndex &parent) const
 {
-  if (parent.isValid())
+  if (parent.isValid()) {
     return QModelIndex();
+  }
   if (!hasIndex(row, column, parent)) {
     return QModelIndex();
   }
   return createIndex(row, column);
 }
 
-QModelIndex FontModel::parent(const QModelIndex& child) const
+QModelIndex FontModel::parent(const QModelIndex &child) const
 {
+  Q_UNUSED(child);
   return QModelIndex();
 }
 
-int FontModel::columnCount(const QModelIndex& parent) const
+int FontModel::columnCount(const QModelIndex &parent) const
 {
+  Q_UNUSED(parent);
   return 6;
 }
 
-Qt::ItemFlags FontModel::flags(const QModelIndex& index) const
+Qt::ItemFlags FontModel::flags(const QModelIndex &index) const
 {
   const Qt::ItemFlags defaultFlags = QAbstractItemModel::flags(index);
-  if (index.column() < 2)
+  if (index.column() < 2) {
     return defaultFlags;
-  if (index.column() == 2)
+  }
+  if (index.column() == 2) {
     return defaultFlags | Qt::ItemIsEditable;
+  }
   return defaultFlags | Qt::ItemIsUserCheckable;
 }
 
-QVariant FontModel::data(const QModelIndex& index, int role) const
+QVariant FontModel::data(const QModelIndex &index, int role) const
 {
   if (index.column() == 0) {
-    if (role == Qt::DisplayRole)
+    if (role == Qt::DisplayRole) {
       return m_text.isEmpty() ? "<no text>" : m_text;
-    if (role == Qt::FontRole)
+    }
+    if (role == Qt::FontRole) {
       return m_fonts.at(index.row());
+    }
   } else if (index.column() == 1) {
-    if (role == Qt::DisplayRole)
+    if (role == Qt::DisplayRole) {
       return m_fonts.at(index.row()).family();
+    }
   } else if (index.column() == 2) {
-    if (role == Qt::DisplayRole || role == Qt::EditRole)
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
       return m_fonts.at(index.row()).pointSize();
+    }
   } else if (index.column() == 3) {
-    if (role == Qt::CheckStateRole)
+    if (role == Qt::CheckStateRole) {
       return m_fonts.at(index.row()).bold() ? Qt::Checked : Qt::Unchecked;
+    }
   } else if (index.column() == 4) {
-    if (role == Qt::CheckStateRole)
+    if (role == Qt::CheckStateRole) {
       return m_fonts.at(index.row()).italic() ? Qt::Checked : Qt::Unchecked;
+    }
   } else if (index.column() == 5) {
-    if (role == Qt::CheckStateRole)
+    if (role == Qt::CheckStateRole) {
       return m_fonts.at(index.row()).underline() ? Qt::Checked : Qt::Unchecked;
+    }
   }
 
   return QVariant();
 }
 
-bool FontModel::setData(const QModelIndex& index, const QVariant& value, int role)
+bool FontModel::setData(const QModelIndex &index, const QVariant &value, int role)
 {
   if (index.column() == 2 && role == Qt::EditRole) {
     m_fonts[index.row()].setPointSize(value.toInt());
--- tools/fontbrowser/fontmodel.h
+++ tools/fontbrowser/fontmodel.h
@@ -21,7 +21,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #ifndef FONTMODEL_H
 #define FONTMODEL_H
 
@@ -34,27 +33,38 @@ namespace Endoscope
 class FontModel : public QAbstractItemModel
 {
   Q_OBJECT
-public:
-  FontModel(QObject *parent);
+  public:
+    FontModel(QObject *parent);
 
-  void updateFonts(const QList<QFont> &fonts);
-  QList<QFont> currentFonts() const;
+    void updateFonts(const QList<QFont> &fonts);
+    QList<QFont> currentFonts() const;
 
-  virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
-  virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-  virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
-  virtual bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
-  virtual QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const;
-  virtual QModelIndex parent(const QModelIndex& child) const;
-  virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
-  virtual Qt::ItemFlags flags(const QModelIndex& index) const;
+    virtual QVariant headerData(int section,
+                                Qt::Orientation orientation,
+                                int role = Qt::DisplayRole) const;
 
-public slots:
-  void updateText(const QString &text);
+    virtual QVariant data(const QModelIndex &index,
+                          int role = Qt::DisplayRole) const;
 
-private:
-  QList<QFont> m_fonts;
-  QString m_text;
+    virtual bool setData(const QModelIndex &index, const QVariant &value,
+                         int role = Qt::EditRole);
+
+    virtual QModelIndex index(int row, int column,
+                              const QModelIndex &parent = QModelIndex()) const;
+
+    virtual QModelIndex parent(const QModelIndex &child) const;
+
+    virtual int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+
+  public slots:
+    void updateText(const QString &text);
+
+  private:
+    QList<QFont> m_fonts;
+    QString m_text;
 };
 
 }
