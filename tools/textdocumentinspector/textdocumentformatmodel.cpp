c0f387f4124b3ea53b0c7d356aa22b7db2e7e211
Allen Winter <allen.winter@kdab.com>
2011-09-04 12:01:43 -0400

    Q_UNUSED() coding style fixes

--- tools/textdocumentinspector/textdocumentformatmodel.cpp
+++ tools/textdocumentinspector/textdocumentformatmodel.cpp
@@ -29,54 +29,65 @@ using namespace Endoscope;
 
 static QMetaEnum propertyEnum()
 {
-  const int index = QTextFormat::staticMetaObject.indexOfEnumerator( "Property" );
-  Q_ASSERT( index >= 0 );
-  return QTextFormat::staticMetaObject.enumerator( index );
+  const int index = QTextFormat::staticMetaObject.indexOfEnumerator("Property");
+  Q_ASSERT(index >= 0);
+  return QTextFormat::staticMetaObject.enumerator(index);
 }
 
-TextDocumentFormatModel::TextDocumentFormatModel(QObject* parent) : QAbstractTableModel(parent)
+TextDocumentFormatModel::TextDocumentFormatModel(QObject *parent)
+  : QAbstractTableModel(parent)
 {
 }
 
-void TextDocumentFormatModel::setFormat(const QTextFormat& format)
+void TextDocumentFormatModel::setFormat(const QTextFormat &format)
 {
   beginResetModel();
   m_format = format;
   endResetModel();
 }
 
-int TextDocumentFormatModel::rowCount(const QModelIndex& parent) const
+int TextDocumentFormatModel::rowCount(const QModelIndex &parent) const
 {
-  if ( !m_format.isValid() || parent.isValid() )
+  if (!m_format.isValid() || parent.isValid()) {
     return 0;
+  }
   return propertyEnum().keyCount();
 }
 
-int TextDocumentFormatModel::columnCount(const QModelIndex& parent) const
+int TextDocumentFormatModel::columnCount(const QModelIndex &parent) const
 {
+  Q_UNUSED(parent);
   return 3;
 }
 
-QVariant TextDocumentFormatModel::data(const QModelIndex& index, int role) const
+QVariant TextDocumentFormatModel::data(const QModelIndex &index, int role) const
 {
-  if ( role == Qt::DisplayRole && index.isValid() ) {
-    const int enumValue = propertyEnum().value( index.row() );
-    switch ( index.column() ) {
-      case 0: return QString::fromLatin1( propertyEnum().key( index.row() ) );
-      case 1: return Util::variantToString( m_format.property( enumValue ) );
-      case 2: return QString::fromLatin1( m_format.property( enumValue ).typeName() );
+  if (role == Qt::DisplayRole && index.isValid()) {
+    const int enumValue = propertyEnum().value(index.row());
+    switch (index.column()) {
+    case 0:
+      return QString::fromLatin1(propertyEnum().key(index.row()));
+    case 1:
+      return Util::variantToString(m_format.property(enumValue));
+    case 2:
+      return QString::fromLatin1(m_format.property(enumValue).typeName());
     }
   }
   return QVariant();
 }
 
-QVariant TextDocumentFormatModel::headerData(int section, Qt::Orientation orientation, int role) const
+QVariant TextDocumentFormatModel::headerData(int section,
+                                             Qt::Orientation orientation,
+                                             int role) const
 {
-  if ( orientation == Qt::Horizontal && role == Qt::DisplayRole ) {
-    switch ( section ) {
-      case 0: return tr("Property");
-      case 1: return tr("Value");
-      case 2: return tr("Type");
+  if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
+    switch (section) {
+    case 0:
+      return tr("Property");
+    case 1:
+      return tr("Value");
+    case 2:
+      return tr("Type");
     }
   }
   return QAbstractItemModel::headerData(section, orientation, role);
--- tools/textdocumentinspector/textdocumentformatmodel.h
+++ tools/textdocumentinspector/textdocumentformatmodel.h
@@ -33,13 +33,13 @@ class TextDocumentFormatModel : public QAbstractTableModel
 {
   Q_OBJECT
   public:
-    explicit TextDocumentFormatModel( QObject * parent = 0 );
+    explicit TextDocumentFormatModel(QObject *parent = 0);
 
-    void setFormat( const QTextFormat &format );
+    void setFormat(const QTextFormat &format);
 
-    int rowCount(const QModelIndex& parent = QModelIndex()) const;
-    int columnCount(const QModelIndex& parent = QModelIndex()) const;
-    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 
   private:
