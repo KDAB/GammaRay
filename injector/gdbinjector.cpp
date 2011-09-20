b7c5e0c86a37dd758331e95279978be2f7933c31
Milian Wolff <mail@milianw.de>
2011-09-20 13:34:55 +0200

    also output error when the QtCore lib is missing debug symbols and gdb injector is used
    TODO: properly quit debuggee app, requires partial rewrite of the gdbinjector though,
    to not dump all commands at once. instead we'd probably be better off using the gdb/mi interface

--- injector/gdbinjector.cpp
+++ injector/gdbinjector.cpp
@@ -134,21 +134,30 @@ void GdbInjector::readyReadStandardError()
 {
   const QString error = m_process->readAllStandardError();
 
-  if (error.startsWith(QLatin1String("Function \"main\" not defined."))) {
-    m_process->kill();
-    disconnect(m_process.data(), SIGNAL(readyReadStandardError()), this, 0);
-    disconnect(m_process.data(), SIGNAL(readyReadStandardOutput()), this, 0);
 
+
+  if (error.startsWith(QLatin1String("Function \"main\" not defined."))) {
     mManualError = true;
-    mProcessError = QProcess::FailedToStart;
     mErrorString = tr("The debuggee application is missing debug symbols which are required\n"
                       "for Endoscope's GDB injector. Please recompile the debuggee.\n\n"
                       "GDB error was: %1").arg(error);
+  } else if (error.startsWith(QLatin1String("Can't find member of namespace, class, struct, or union named \"QCoreApplication::exec\""))) {
+    mManualError = true;
+    mErrorString = tr("Your QtCore library is missing debug symbols which are required\n"
+                      "for Endoscope's GDB injector. Please install the required debug symbols.\n\n"
+                      "GDB error was: %1").arg(error);
+  }
+
+  if (mManualError) {
+    m_process->kill();
+    disconnect(m_process.data(), SIGNAL(readyReadStandardError()), this, 0);
+    disconnect(m_process.data(), SIGNAL(readyReadStandardOutput()), this, 0);
+    mProcessError = QProcess::FailedToStart;
     return;
   }
 
   static QTextStream out(stderr);
-  out << "ERROR: " << error << flush;
+  out << error << flush;
 }
 
 void GdbInjector::readyReadStandardOutput()
