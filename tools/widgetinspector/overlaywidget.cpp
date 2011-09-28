012fcaec903156b700a40cd61c2e46f98a8974b0
Allen Winter <allen.winter@kdab.com>
2011-09-28 10:12:54 -0400

    Gammaray => GammaRay minor in comment

--- tools/widgetinspector/overlaywidget.cpp
+++ tools/widgetinspector/overlaywidget.cpp
@@ -1,7 +1,7 @@
 /*
   overlaywidget.cpp
 
-  This file is part of Gammaray, the Qt application inspection and
+  This file is part of GammaRay, the Qt application inspection and
   manipulation tool.
 
   Copyright (C) 2010-2011 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
