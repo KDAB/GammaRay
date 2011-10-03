2b0adb8e301d8b647c2c6ffaf2b9518a85506a37
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-10-03 13:01:04 +0200

    use just the remote thread injection method, because it works for 32 and 64 bit and its more higher level, so it can not break so quickly

--- injector/windllinjector.cpp
+++ injector/windllinjector.cpp
@@ -91,70 +91,7 @@ bool WinDllInjector::attach(int pid, const QString &probeDll, const QString &/*p
     return false;
   }
 
-  return inject2();
-}
-
-bool WinDllInjector::inject()
-{
-  CONTEXT     context;
-
-  void       *mem             = NULL;
-  size_t      memLen          = 0;
-  UINT_PTR    fnLoadLibrary   = NULL;
-
-  size_t      codeSize = 20;
-
-  ::ZeroMemory(&context, sizeof(CONTEXT));
-
-  if(m_destProcess == NULL || m_destThread == NULL) {
-    qDebug() << "destination process or thread are empty: process"
-             << m_destProcess << "thread" << m_destThread;
-    return false;
-  }
-
-  if (::GetFileAttributes((WCHAR*)m_dllPath.utf16()) == INVALID_FILE_ATTRIBUTES) {
-    qDebug() << "invalid file attributes for file" << m_dllPath;
-    return false;
-  }
-
-  BYTE *code = new BYTE[codeSize + (m_dllPath.length() + 1) * sizeof(wchar_t)];
-
-  memLen = (m_dllPath.length() + 1) * sizeof(wchar_t);
-  CopyMemory(code + codeSize, m_dllPath.utf16(), memLen);
-  memLen += codeSize;
-
-  context.ContextFlags = CONTEXT_FULL;
-  GetThreadContext(m_destThread, &context);
-
-  mem = VirtualAllocEx(m_destProcess, NULL, memLen, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-  fnLoadLibrary = (UINT_PTR)GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")), "LoadLibraryW");
-
-  union
-  {
-    PBYTE  pB;
-    PINT   pI;
-    PULONGLONG pL;
-  } ip;
-
-  ip.pB = code;
-
-  *ip.pB++ = 0x68;            // push  eip
-  *ip.pI++ = context.Eip;
-  *ip.pB++ = 0x9c;            // pushf
-  *ip.pB++ = 0x60;            // pusha
-  *ip.pB++ = 0x68;            // push  "path\to\our.dll"
-  *ip.pI++ = (UINT_PTR)mem + codeSize;
-  *ip.pB++ = 0xe8;            // call  LoadLibraryW
-  *ip.pI++ = (UINT_PTR)fnLoadLibrary - ((UINT_PTR)mem + (ip.pB + 4 - code));
-  *ip.pB++ = 0x61;            // popa
-  *ip.pB++ = 0x9d;            // popf
-  *ip.pB++ = 0xc3;            // ret
-
-  WriteProcessMemory(m_destProcess, mem, code, memLen, NULL);
-  FlushInstructionCache(m_destProcess, mem, memLen);
-  context.Eip = (UINT_PTR)mem;
-  SetThreadContext(m_destThread, &context);
-  return true;
+  return inject();
 }
 
 int WinDllInjector::exitCode()
@@ -172,7 +109,7 @@ QProcess::ExitStatus WinDllInjector::exitStatus()
   return mExitStatus;
 }
 
-bool WinDllInjector::inject2()
+bool WinDllInjector::inject()
 {
   int strsize = (m_dllPath.size() * 2) + 2;
   void *mem = VirtualAllocEx(m_destProcess, NULL, strsize, MEM_COMMIT, PAGE_READWRITE);
