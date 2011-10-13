91e80051f2c694839249e872e18b32615740d3ee
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-10-13 20:45:43 +0200

    add backtrace handling for windows to messagehandler

--- tools/messagehandler/backtrace_win.cpp
+++ tools/messagehandler/backtrace_win.cpp
@@ -22,10 +22,36 @@
 */
 
 #include "backtrace.h"
+#include "StackWalker.h"
 
-///FIXME: implement this, e.g. by using http://stackwalker.codeplex.com/
-
-Backtrace getBacktrace(int levels)
+class StackWalkerToQStringList : public StackWalker
 {
-  return Backtrace();
+public:
+  QStringList getStackWalkerBacktrace() {
+      m_stackTrace.clear();
+      ShowCallstack();
+      return m_stackTrace;
+  }
+
+protected:
+  virtual void OnOutput(LPCSTR szText)
+  {
+    m_stackTrace << szText;
+  }
+  virtual void OnSymInit(LPCSTR szSearchPath, DWORD symOptions, LPCSTR szUserName){}
+  virtual void OnLoadModule(LPCSTR img, LPCSTR mod, DWORD64 baseAddr, DWORD size, DWORD result, LPCSTR symType, LPCSTR pdbName, ULONGLONG fileVersion){}
+  virtual void OnDbgHelpErr(LPCSTR szFuncName, DWORD gle, DWORD64 addr){}
+
+private:
+    QStringList m_stackTrace;
+};
+
+static StackWalkerToQStringList *stackWalkerToQStringList = 0;
+
+Backtrace getBacktrace(int /*levels*/)
+{
+    //FIXME: Perhaps take the levels into account
+    if (!stackWalkerToQStringList)
+        stackWalkerToQStringList = new StackWalkerToQStringList();
+    return stackWalkerToQStringList->getStackWalkerBacktrace();
 }
