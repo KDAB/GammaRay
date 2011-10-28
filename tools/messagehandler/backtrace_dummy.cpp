b84fc9c59101ef15c38a7f88e6f60c009d717ce6
Volker Krause <volker.krause@kdab.com>
2011-10-28 15:36:15 +0200

    StackWalker doesn't work with mingw.

---
+++ tools/messagehandler/backtrace_dummy.cpp
@@ -0,0 +1,30 @@
+/*
+  backtrace_dummy.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2011 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "backtrace.h"
+
+Backtrace getBacktrace(int levels)
+{
+  Q_UNUSED(levels);
+  return Backtrace();
+}
