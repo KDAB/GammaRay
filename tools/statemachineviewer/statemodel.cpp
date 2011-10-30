981759c6c283d9df760a2038633d3de68bf1c66e
Volker Krause <volker.krause@kdab.com>
2011-10-30 11:38:29 +0100

    The correct type Qt::CheckStateRole is Qt::CheckState, not bool.

--- tools/statemachineviewer/statemodel.cpp
+++ tools/statemachineviewer/statemodel.cpp
@@ -169,7 +169,7 @@ QVariant StateModel::data(const QModelIndex &index, int role) const
     if (index.column() == 0 && role == Qt::CheckStateRole) {
       QState *s = qobject_cast<QState*>(obj);
       if (s) {
-        return d->m_stateMachine->configuration().contains(s);
+        return d->m_stateMachine->configuration().contains(s) ? Qt::Checked : Qt::Unchecked;
       }
     }
 
