708993b197703c8a54f64777f7c258ae1da2f184
Volker Krause <volker.krause@kdab.com>
2012-01-06 14:32:02 +0100

    Add empty shell for the style inspector.

---
+++ tools/styleinspector/styleinspector.cpp
@@ -0,0 +1,13 @@
+#include "styleinspector.h"
+
+using namespace GammaRay;
+
+StyleInspector::StyleInspector(ProbeInterface* probe, QWidget* parent): QWidget(parent)
+{
+}
+
+StyleInspector::~StyleInspector()
+{
+}
+
+#include "styleinspector.moc"
---
+++ tools/styleinspector/styleinspector.h
@@ -0,0 +1,31 @@
+#ifndef GAMMARAY_STYLEINSPECTOR_H
+#define GAMMARAY_STYLEINSPECTOR_H
+
+#include <toolfactory.h>
+
+#include <qstyle.h>
+#include <qwidget.h>
+
+namespace GammaRay {
+
+class StyleInspector : public QWidget
+{
+  Q_OBJECT
+public:
+  explicit StyleInspector(ProbeInterface *probe, QWidget *parent = 0);
+  virtual ~StyleInspector();
+};
+
+class StyleInspectorFactory : public QObject, public StandardToolFactory<QStyle, StyleInspector>
+{
+  Q_OBJECT
+  Q_INTERFACES(GammaRay::ToolFactory)
+
+public:
+  explicit StyleInspectorFactory(QObject* parent = 0) : QObject(parent) {}
+  virtual QString name() const { return tr("Style"); }
+};
+
+}
+
+#endif // GAMMARAY_STYLEINSPECTOR_H
