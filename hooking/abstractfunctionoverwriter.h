701c6abc0c3e4cd5956331cf44ffb5ec4452bac1
Milian Wolff <mail@milianw.de>
2012-04-30 20:35:18 +0200

    Revert "fix compilation: error: 'intptr_t' has not been declared (include stdint.h)"
    This reverts commit a61e7b5749841907ef94e0011f2bdd0fa7239a0a.
    
    Still not compiling for me, even with this patch:
    
    error: ‘INTPTR_MIN’ was not declared in this scope
    error: ‘INTPTR_MAX’ was not declared in this scope
    
    I wonder how this is supposed to work...

--- hooking/abstractfunctionoverwriter.h
+++ hooking/abstractfunctionoverwriter.h
@@ -25,8 +25,6 @@
 #include <QList>
 #include <QString>
 
-#include <stdint.h>
-
 #if defined(_M_X64) || defined(__amd64) || defined(__x86_64)
 #define ARCH_64
 #elif defined(_M_IX86) || defined(__i386__)
