5fc0574792ddf8fd10bc83e3d63181d962cf4cae
Allen Winter <allen.winter@kdab.com>
2012-06-17 16:57:39 -0400

    update copyright year to 2012

--- core/tools/modelinspector/modelcellmodel.cpp
+++ core/tools/modelinspector/modelcellmodel.cpp
@@ -4,7 +4,7 @@
   This file is part of GammaRay, the Qt application inspection and
   manipulation tool.
 
-  Copyright (C) 2010-2011 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Copyright (C) 2010-2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
   Author: Volker Krause <volker.krause@kdab.com>
 
   This program is free software; you can redistribute it and/or modify
