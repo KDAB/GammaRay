eced72a57a0bf260b78af2b83709f3c8df8107a5
Volker Krause <volker.krause@kdab.com>
2012-07-25 17:45:23 +0200

    Support all QObject sub-classes in variantToString() (Qt5 only). (cherry picked from commit f9746aed1c952d9af8b5f5d228935e97ec8f3ffc)

--- core/util.cpp
+++ core/util.cpp
@@ -216,6 +216,7 @@ QString GammaRay::Util::variantToString(const QVariant &value)
     return QObject::tr("<%1 elements>").arg(path.elementCount());
   }
 
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
   if (value.type() == (QVariant::Type)qMetaTypeId<QWidget*>()) {
     return displayString(value.value<QWidget*>());
   }
@@ -238,6 +239,11 @@ QString GammaRay::Util::variantToString(const QVariant &value)
   if (value.userType() == qMetaTypeId<const QStyle*>()) {
     return displayString(value.value<const QStyle*>());
   }
+#else
+  if (value.canConvert<QObject*>()) {
+    return displayString(value.value<QObject*>());
+  }
+#endif
 
   // enums
   const QString enumStr = enumToString(value);
