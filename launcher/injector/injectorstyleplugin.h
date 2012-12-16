4773c93529f71ddb85ed50260605f90a4995d36f
Volker Krause <volker.krause@kdab.com>
2012-12-16 15:46:16 +0100

    Make the style plugin loadable with Qt5.

--- launcher/injector/injectorstyleplugin.h
+++ launcher/injector/injectorstyleplugin.h
@@ -31,6 +31,9 @@ namespace GammaRay {
 class InjectorStylePlugin : public QStylePlugin
 {
   Q_OBJECT
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+  Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QStyleFactoryInterface" FILE "injectorstyle.json")
+#endif
   public:
     QStyle *create(const QString &);
     QStringList keys() const;
