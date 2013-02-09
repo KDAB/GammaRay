3bbac13396c45906d3cdeab9f1fad2a481613f94
Volker Krause <volker.krause@kdab.com>
2013-02-09 14:06:02 +0100

    Register an object locally as well.
    Enables code that is shared between client and server to work, as well as
    the in-process UI.

--- core/remote/objectserver.cpp
+++ core/remote/objectserver.cpp
@@ -1,11 +1,14 @@
 #include "objectserver.h"
 #include "server.h"
 
+#include <network/objectbroker.h>
+
 using namespace GammaRay;
 
 ObjectServer::ObjectServer(const QString& objectName, QObject* parent) : NetworkObject(objectName, parent)
 {
   m_myAddress = Server::instance()->registerObject(objectName, this, "newMessage");
+  ObjectBroker::registerObject(objectName, this);
 }
 
 ObjectServer::~ObjectServer()
