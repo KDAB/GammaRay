7e576b796c1dede725d2968ff85934794e26d9de
Kevin Funk <kevin.funk@kdab.com>
2013-03-15 23:00:22 +0100

    Qt5: Remove QT_{BEGIN,END}_HEADER macro usage
    See:
    http://lists.qt-project.org/pipermail/development/2013-January/009284.html

--- 3rdparty/qt/resourcemodel.h
+++ 3rdparty/qt/resourcemodel.h
@@ -46,8 +46,6 @@
 #include <QtCore/qdir.h>
 #include <qfileiconprovider.h>
 
-QT_BEGIN_HEADER
-
 QT_BEGIN_NAMESPACE
 
 QT_MODULE(Gui)
@@ -151,7 +149,5 @@ private:
 };
 
 QT_END_NAMESPACE
-
-QT_END_HEADER
 
 #endif // QDIRMODEL_H
