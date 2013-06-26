6ddc7952b38c370190a24bcd28a57a3a0a386d52
Rafael Roquetto <rafael.roquetto@kdab.com>
2013-06-26 19:02:14 -0300

    Support for BlackBerry 10

---
+++ cmake/Toolchain-blackberry-armv7le.cmake
@@ -0,0 +1,18 @@
+include(Platform/QNX)
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME BlackBerry)
+
+# which compilers to use for C and C++
+SET(arch gcc_ntoarmv7le)
+SET(CMAKE_C_COMPILER qcc -V${arch})
+SET(CMAKE_CXX_COMPILER qcc -V${arch})
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH $ENV{QNX_TARGET}/armle-v7)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
