67a3e23f646e193e242cf616a4a97beff3cd524c
Milian Wolff <milian.wolff@kdab.com>
2013-09-02 18:02:14 +0200

    Add a manual message model test app.

---
+++ tests/manual/messagemodeltest.cpp
@@ -0,0 +1,86 @@
+/*
+  messagemodeltest.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2012-2013 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Milian Wolff <milian.wolff@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "messagemodeltest.h"
+
+#include <QApplication>
+#include <QDebug>
+#include <QVBoxLayout>
+#include <QPushButton>
+
+using namespace GammaRay;
+
+MessageGenerator::MessageGenerator()
+{
+  QVBoxLayout *layout  = new QVBoxLayout;
+
+  QPushButton *button = new QPushButton("debug");
+  connect(button, SIGNAL(clicked(bool)), SLOT(generateDebug()));
+  layout->addWidget(button);
+
+  button = new QPushButton("warning");
+  connect(button, SIGNAL(clicked(bool)), SLOT(generateWarning()));
+  layout->addWidget(button);
+
+  button = new QPushButton("critical");
+  connect(button, SIGNAL(clicked(bool)), SLOT(generateCritical()));
+  layout->addWidget(button);
+
+  button = new QPushButton("fatal");
+  connect(button, SIGNAL(clicked(bool)), SLOT(generateFatal()));
+  layout->addWidget(button);
+
+  setLayout(layout);
+}
+
+void MessageGenerator::generateDebug()
+{
+  qDebug() << "A debug message";
+}
+
+void MessageGenerator::generateWarning()
+{
+  qWarning() << "A warning message" << "splitted in two parts";
+}
+
+void MessageGenerator::generateCritical()
+{
+  qCritical() << "A critical message";
+}
+
+void MessageGenerator::generateFatal()
+{
+  qFatal("A fatal message");
+}
+
+int main(int argc, char **argv)
+{
+  QApplication app(argc, argv);
+
+  MessageGenerator generator;
+  generator.show();
+
+  return app.exec();
+}
+
+#include "messagemodeltest.moc"
---
+++ tests/manual/messagemodeltest.h
@@ -0,0 +1,47 @@
+/*
+  messagemodeltest.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2012-2013 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Milian Wolff <milian.wolff@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_MESSAGEMODELTEST_H
+#define GAMMARAY_MESSAGEMODELTEST_H
+
+#include <QWidget>
+
+namespace GammaRay {
+
+class MessageGenerator : public QWidget
+{
+  Q_OBJECT
+
+  public:
+    MessageGenerator();
+
+  private slots:
+    void generateDebug();
+    void generateWarning();
+    void generateCritical();
+    void generateFatal();
+};
+
+}
+
+#endif // GAMMARAY_MESSAGEMODELTEST_H
