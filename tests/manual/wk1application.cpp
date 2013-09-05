ee0da3da88a4f5d2e69aa9b6198fe852d083a431
Volker Krause <volker.krause@kdab.com>
2013-09-05 13:42:04 +0200

    Add a WebKit1 test application.
    Unintentionally exposes a nasty deadlock between the object registration
    mutex and the one in the QAbstractSocketEngine.

---
+++ tests/manual/wk1application.cpp
@@ -0,0 +1,34 @@
+/*
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2013 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QApplication>
+#include <QWebView>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QWebView view;
+    view.setUrl(QUrl("http://www.kdab.com/gammaray")); 
+    view.show();
+
+    return app.exec();
+}
