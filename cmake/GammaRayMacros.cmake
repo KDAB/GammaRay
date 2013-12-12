15b06dde7e85a0a97e1de8b635e79296100880fb
Volker Krause <volker.krause@kdab.com>
2013-12-12 11:23:06 +0100

    Fix difference in install and build directory layout.
    This fixes plugin loading on Windows with the recent changes to use
    relative search paths.

--- cmake/GammaRayMacros.cmake
+++ cmake/GammaRayMacros.cmake
@@ -12,9 +12,9 @@ macro(gammaray_add_plugin _target_name _desktop_file)
   add_library(${_target_name} MODULE ${ARGN})
   set_target_properties(${_target_name} PROPERTIES
     PREFIX ""
-    LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib/plugins/gammaray
+    LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${PLUGIN_INSTALL_DIR}/gammaray
   )
   install(TARGETS ${_target_name} DESTINATION ${PLUGIN_INSTALL_DIR}/gammaray/)
   install(FILES ${_desktop_file} DESTINATION ${PLUGIN_INSTALL_DIR}/gammaray/)
-  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/${_desktop_file}" "${PROJECT_BINARY_DIR}/lib/plugins/gammaray/${_desktop_file}")
+  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/${_desktop_file}" "${PROJECT_BINARY_DIR}/${PLUGIN_INSTALL_DIR}/gammaray/${_desktop_file}")
 endmacro()
