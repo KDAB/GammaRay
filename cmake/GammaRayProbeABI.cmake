f7b1e737480737fe599ae6386d758965aa980252
Allen Winter <allen.winter@kdab.com>
2013-12-19 18:08:17 -0500

    add KDAB copyright and BSD license

--- cmake/GammaRayProbeABI.cmake
+++ cmake/GammaRayProbeABI.cmake
@@ -1,6 +1,14 @@
 # Determine probe ABI identifier
 #
+
+#  Copyright (c) 2011-2013 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+# Author: Volker Krause <volker.krause@kdab.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+
 # This contains all properties that define ABI compatibility of a probe with a target
+
 # It includes:
 # - Qt version (major/minor)
 # - Compiler (Windows only)
@@ -10,7 +18,7 @@
 if(Qt5Core_FOUND)
   set(GAMMARAY_PROBE_ABI "qt${Qt5Core_VERSION_MAJOR}.${Qt5Core_VERSION_MINOR}")
 else()
- set(GAMMARAY_PROBE_ABI "qt${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}")
+  set(GAMMARAY_PROBE_ABI "qt${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}")
 endif()
 
 if(WIN32)
