7eb2f938174002a8e913708358cbe31d0be0fb3e
Volker Krause <volker.krause@kdab.com>
2013-12-21 11:43:25 +0100

    Add a command line option to connect to an already injected probe.
    This is useful now that gammaray-client is no longer in PATH.

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -46,7 +46,7 @@ QTextStream err(stderr);
 static void usage(const char *argv0)
 {
   out << "Usage: " << argv0
-      << " [--injector <injector>] [--pid <pid> | <application> <args>]" << endl;
+      << " [options] [--pid <pid> | <application> <args> | --connect <host>[:<port>]]" << endl;
   out << "" << endl;
   out << "Inspect runtime internals of a Qt-application, such as:" << endl;
   out << "  QObject tree, properties, signal/slots, widgets, models," << endl;
@@ -54,18 +54,19 @@ static void usage(const char *argv0)
   out << "  state machines, meta types, fonts, codecs, text documents" << endl;
   out << "" << endl;
   out << "Options:" << endl;
-  out << " -i, --injector <injector>\tset injection type, possible values:" << endl;
-  out << "                          \t" << InjectorFactory::availableInjectors().join(", ")
+  out << " -i, --injector <injector>  \tset injection type, possible values:" << endl;
+  out << "                            \t" << InjectorFactory::availableInjectors().join(", ")
       << endl;
-  out << " -p, --pid <pid>          \tattach to running Qt application" << endl;
-  out << "     --inprocess          \tuse in-process UI" << endl;
-  out << "     --inject-only        \tonly inject the probe, don't show the UI" << endl;
-  out << "     --listen <address>   \tspecify the address the server should listen on [default: 0.0.0.0]" << endl;
-  out << "     --no-listen          \tdisables remote access entirely (implies --inprocess)" << endl;
-  out << "     --list-probes        \tlist all installed probes" << endl;
-  out << "     --probe <abi>        \tspecify which probe to use" << endl;
-  out << " -h, --help               \tprint program help and exit" << endl;
-  out << " -v, --version            \tprint program version and exit" << endl;
+  out << " -p, --pid <pid>            \tattach to running Qt application" << endl;
+  out << "     --inprocess            \tuse in-process UI" << endl;
+  out << "     --inject-only          \tonly inject the probe, don't show the UI" << endl;
+  out << "     --listen <address>     \tspecify the address the server should listen on [default: 0.0.0.0]" << endl;
+  out << "     --no-listen            \tdisables remote access entirely (implies --inprocess)" << endl;
+  out << "     --list-probes          \tlist all installed probes" << endl;
+  out << "     --probe <abi>          \tspecify which probe to use" << endl;
+  out << "     --connect <host>[:port]\tconnect to an already injected target" << endl;
+  out << " -h, --help                 \tprint program help and exit" << endl;
+  out << " -v, --version              \tprint program version and exit" << endl;
 #ifdef HAVE_QT_WIDGETS
   out << endl
       << "When run without any options, " << argv0 << " will present a list of running\n"
@@ -153,6 +154,19 @@ int main(int argc, char **argv)
         return 1;
       }
       options.setProbeABI(abi);
+    }
+    if ( arg == QLatin1String("--connect") && !args.isEmpty()) {
+      QString host = args.takeFirst();
+      quint16 port = 0;
+      const int pos = host.lastIndexOf(":");
+      if (pos > 0) {
+        port = host.mid(pos + 1).toUShort();
+        host = host.left(pos);
+      }
+      ClientLauncher client;
+      client.launch(host, port);
+      client.waitForFinished();
+      return 0;
     }
 
     // debug/test options
