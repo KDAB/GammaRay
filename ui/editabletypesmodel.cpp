1416fe39869dc3701c6ab4a1282716b17802b787
Volker Krause <volker.krause@kdab.com>
2014-01-04 19:28:54 +0100

    Add a model for all types we can edit.

---
+++ ui/editabletypesmodel.cpp
@@ -0,0 +1,58 @@
+/*
+  editabletypesmodel.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "editabletypesmodel.h"
+#include "propertyeditor/propertyeditorfactory.h"
+
+using namespace GammaRay;
+
+EditableTypesModel::EditableTypesModel(QObject* parent): QAbstractListModel(parent)
+{
+  m_types = PropertyEditorFactory::supportedTypes();
+}
+
+EditableTypesModel::~EditableTypesModel()
+{
+}
+
+int EditableTypesModel::rowCount(const QModelIndex& parent) const
+{
+  if (parent.isValid())
+    return 0;
+  return m_types.size();
+}
+
+QVariant EditableTypesModel::data(const QModelIndex& index, int role) const
+{
+  if (!index.isValid())
+    return QVariant();
+
+  const int type = m_types.at(index.row());
+  if (role == Qt::DisplayRole) {
+    return QMetaType::typeName(type);
+  } else if (role == Qt::UserRole) {
+    return type;
+  }
+
+  return QVariant();
+}
---
+++ ui/editabletypesmodel.h
@@ -0,0 +1,49 @@
+/*
+  editabletypesmodel.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_EDITABLETYPESMODEL_H
+#define GAMMARAY_EDITABLETYPESMODEL_H
+
+#include <QAbstractListModel>
+#include <QVector>
+
+namespace GammaRay {
+
+/** All types we have edit widgets for. */
+class EditableTypesModel : public QAbstractListModel
+{
+    Q_OBJECT
+  public:
+    explicit EditableTypesModel(QObject *parent = 0);
+    ~EditableTypesModel();
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+
+  private:
+    QVector<int> m_types;
+};
+
+}
+
+#endif // GAMMARAY_EDITABLETYPESMODEL_H
