68617e5d1209ffe78fecd4452e729c38cbec6d69
Kevin Funk <kevin.funk@kdab.com>
2014-02-26 13:12:41 +0100

    StateModel: Don't hide final states

--- plugins/statemachineviewer/statemodel.cpp
+++ plugins/statemachineviewer/statemodel.cpp
@@ -79,7 +79,7 @@ QList<QObject*> StateModelPrivate::children(QObject *parent) const
   }
 
   foreach (QObject *o, parent->children()) {
-    if (o->inherits("QState")) {
+    if (o->inherits("QAbstractState")) {
       result.append(o);
     }
   }
@@ -210,7 +210,7 @@ QVariant StateModel::data(const QModelIndex &index, int role) const
     }
 
     if (index.column() == 0 && role == Qt::CheckStateRole) {
-      QState *s = qobject_cast<QState*>(obj);
+      QAbstractState *s = qobject_cast<QAbstractState*>(obj);
       if (s) {
         return d->m_stateMachine->configuration().contains(s) ? Qt::Checked : Qt::Unchecked;
       }
