ab395a5a2bc28560623d3c593caa4239f499f804
Volker Krause <volker.krause@kdab.com>
2014-03-19 13:15:51 +0100

    Find QtCore on Mac, using otool and lsof.

--- common/probeabidetector_mac.cpp
+++ common/probeabidetector_mac.cpp
@@ -26,21 +26,80 @@
 #include "probeabidetector.h"
 #include "probeabi.h"
 
+#include <QDebug>
+#include <QProcess>
 #include <QString>
+
+#include <mach-o/loader.h>
 
 using namespace GammaRay;
 
+static QString qtCoreFromOtool(const QString &path)
+{
+  QProcess proc;
+  proc.setProcessChannelMode(QProcess::SeparateChannels);
+  proc.setReadChannel(QProcess::StandardOutput);
+  proc.start("otool", QStringList() << "-L" << path);
+  proc.waitForFinished();
+
+  forever {
+    const QByteArray line = proc.readLine();
+    if (line.isEmpty())
+      break;
+
+    if (line.contains("QtCore") || line.contains("Qt5Core")) {
+      const int pos = line.lastIndexOf(" (");
+      if (pos <= 0)
+        continue;
+      return QString::fromLocal8Bit(line.left(pos).trimmed());
+    }
+  }
+
+  return QString();
+}
+
 ProbeABI ProbeABIDetector::abiForExecutable(const QString& path) const
 {
-  return ProbeABI::fromString(GAMMARAY_PROBE_ABI);
+  const QString qtCorePath = qtCoreFromOtool(path);
+  if (qtCorePath.isEmpty())
+    return ProbeABI();
+
+  return abiForQtCore(qtCorePath);
+}
+
+
+static QString qtCoreFromLsof(qint64 pid)
+{
+  QProcess proc;
+  proc.setProcessChannelMode(QProcess::SeparateChannels);
+  proc.setReadChannel(QProcess::StandardOutput);
+  proc.start("lsof", QStringList() << "-Fn" << "-n" << "-p" << QString::number(pid));
+  proc.waitForFinished();
+
+  forever {
+    const QByteArray line = proc.readLine();
+    if (line.isEmpty())
+      break;
+
+    if (line.contains("QtCore") || line.contains("Qt5Core")) {
+      return QString::fromLocal8Bit(line.mid(1).trimmed()); // strip the field identifier
+    }
+  }
+
+  return QString();
 }
 
 ProbeABI ProbeABIDetector::abiForProcess(qint64 pid) const
 {
-  return ProbeABI::fromString(GAMMARAY_PROBE_ABI);
+  const QString qtCorePath = qtCoreFromLsof(pid);
+  if (qtCorePath.isEmpty())
+    return ProbeABI();
+
+  return abiForQtCore(qtCorePath);
 }
 
 ProbeABI ProbeABIDetector::detectAbiForQtCore(const QString& path) const
 {
+  qWarning() << path;
   return ProbeABI::fromString(GAMMARAY_PROBE_ABI);
 }
