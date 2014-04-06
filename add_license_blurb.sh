5488e0f548ef589f249a07fa5d9d11c43490d04f
Volker Krause <volker.krause@kdab.com>
2014-04-06 11:18:18 +0200

    Don't hardcode my name/email for the CMakeLists.txt license either.

--- add_license_blurb.sh
+++ add_license_blurb.sh
@@ -44,8 +44,8 @@ find "$@" -name 'CMakeLists.txt' | while read FILE; do
 #  This file is part of GammaRay, the Qt application inspection and
 #  manipulation tool.
 #
-#  Copyright (C) 2010-2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
-#  Author: Volker Krause <volker.krause@kdab.com>
+#  Copyright (C) $thisYear Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+#  Author: $authorName <$authorEmail>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
