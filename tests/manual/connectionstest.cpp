4848bd4998ee98243abb28e06da5465bd8601c8f
Volker Krause <volker.krause@kdab.com>
2014-04-20 22:28:24 +0200

    Add dummy target for testing the connections view.

---
+++ tests/manual/connectionstest.cpp
@@ -0,0 +1,61 @@
+/*
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2013-2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QCoreApplication>
+
+class MyTestObject : public QObject
+{
+    Q_OBJECT
+public slots:
+    void mySlot1() {}
+    void mySlot2() {}
+    void mySlot3() {}
+    void mySlot4() {}
+
+signals:
+    void mySignal1();
+    void mySignal2();
+    void mySignal3();
+    void mySignal4();
+};
+
+static void connectObjects(QObject *sender, QObject *receiver)
+{
+    QObject::connect(sender, SIGNAL(mySignal1()), receiver, SLOT(mySlot1()));
+    QObject::connect(sender, SIGNAL(mySignal2()), receiver, SLOT(mySlot2()), Qt::DirectConnection);
+    QObject::connect(sender, SIGNAL(mySignal3()), receiver, SLOT(mySlot3()), Qt::QueuedConnection);
+    QObject::connect(sender, SIGNAL(mySignal4()), receiver, SLOT(mySlot4()), Qt::BlockingQueuedConnection);
+}
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+
+    MyTestObject sender, receiver;
+    sender.setObjectName("sender");
+    receiver.setObjectName("receiver");
+    connectObjects(&sender, &receiver);
+
+    return app.exec();
+}
+
+#include "connectionstest.moc"
+
