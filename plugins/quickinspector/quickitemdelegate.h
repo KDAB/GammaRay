06515791b0d85d0a043fd5b0cee9ad9e85b951a6
Allen Winter <allen.winter@kdab.com>
2014-06-14 12:27:26 -0400

    add license and copyright header

--- plugins/quickinspector/quickitemdelegate.h
+++ plugins/quickinspector/quickitemdelegate.h
@@ -1,5 +1,28 @@
-#ifndef QUICKITEMDELEGATE_H
-#define QUICKITEMDELEGATE_H
+/*
+  quickitemdelegate.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Anton Kreuzkamp <anton.kreuzkamp@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_QUICKITEMDELEGATE_H
+#define GAMMARAY_QUICKITEMDELEGATE_H
 
 #include <QStyledItemDelegate>
 #include <QModelIndex>
