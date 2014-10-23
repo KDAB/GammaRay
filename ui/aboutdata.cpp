ee2cb3e3d9c32d09e6a50a7bb8ca528d04c88737
Allen Winter <allen.winter@kdab.com>
2014-10-23 18:17:08 -0400

    add license+copyright header

--- ui/aboutdata.cpp
+++ ui/aboutdata.cpp
@@ -1,5 +1,28 @@
-#include <config-gammaray-version.h>
+/*
+  aboutdata.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "aboutdata.h"
+#include <config-gammaray-version.h>
 
 #include <QFile>
 #include <QTextDocument>
@@ -11,7 +34,7 @@ QStringList AboutData::authors()
     QFile f(":/gammaray/authors");
     f.open(QFile::ReadOnly);
     Q_ASSERT(f.isOpen());
-    return QString::fromUtf8(f.readAll()).split("\n", QString::SkipEmptyParts);
+    return QString::fromUtf8(f.readAll()).split('\n', QString::SkipEmptyParts);
 }
 
 QStringList AboutData::authorsAsHtml()
