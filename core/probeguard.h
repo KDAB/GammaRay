55c6d5a8f2266a393868f949cc736b6eeb707929
Volker Krause <volker.krause@kdab.com>
2014-12-18 08:51:31 +0100

    Introduce ProbeGuard.
    This is a simple RAII class to mark code that is part of the GammaRay
    probe. This allows us to check in a few places where we are doing
    expensive or otherwise problematic operations inside the probe if we
    are dealing with activity from the probe itself (which thus can be
    ignored), or activity from the application itself.
    
    This improves performance, obviously, and the quality of the results, as
    it allows for example to separate GammaRay debug output from the one of
    the target application.

---
+++ core/probeguard.h
@@ -0,0 +1,55 @@
+/*
+  probeguard.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2014 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_PROBEGUARD_H
+#define GAMMARAY_PROBEGUARD_H
+
+#include "gammaray_core_export.h"
+
+namespace GammaRay {
+
+/** Use this inside probe code that might trigger expensive or otherwise problematic side-effects in
+ * GammaRay itself, so the triggered probe part can skip reacting to that action.
+ *
+ * This works per-thread, and is supposed to be used RAII-style.
+ *
+ * Example: Creating a short-lived QObject inside a probe plugin.
+ */
+class GAMMARAY_CORE_EXPORT ProbeGuard
+{
+public:
+    ProbeGuard();
+    ~ProbeGuard();
+
+    /** Use this inside your probe code to check if you have been called from other probe code.
+     *  In that case you might want to skip some operations,
+     */
+    static bool insideProbe();
+private:
+    Q_DISABLE_COPY(ProbeGuard)
+    void setInsideProbe(bool inside);
+    bool m_previousState;
+};
+}
+
+#endif // GAMMARAY_PROBEGUARD_H
