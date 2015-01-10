8c8f4d500b25668d5606400e9776eae14552708b
Volker Krause <volker.krause@kdab.com>
2015-01-10 15:39:45 +0100

    Add override.

--- core/objectstaticpropertymodel.h
+++ core/objectstaticpropertymodel.h
@@ -34,15 +34,15 @@ class ObjectStaticPropertyModel : public ObjectPropertyModel
   public:
     explicit ObjectStaticPropertyModel(QObject *parent = 0);
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) Q_DECL_OVERRIDE;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
     int columnCount(const QModelIndex& parent = QModelIndex()) const Q_DECL_OVERRIDE;
-    Qt::ItemFlags flags(const QModelIndex &index) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
 
-    void monitorObject(QObject* obj);
-    void unmonitorObject(QObject* obj);
+    void monitorObject(QObject* obj) Q_DECL_OVERRIDE;
+    void unmonitorObject(QObject* obj) Q_DECL_OVERRIDE;
 
   private slots:
     void propertyUpdated();
