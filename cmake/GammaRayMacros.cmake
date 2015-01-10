73731fd6d8a304a2f72c50c4a0bb31c5cb17933f
Volker Krause <volker.krause@kdab.com>
2015-01-10 21:30:29 +0100

    Introduce PROBE_PLUGIN_INSTALL_DIR.
    This centralizes the probe plugin destination from several places all over
    the build system.

--- cmake/GammaRayMacros.cmake
+++ cmake/GammaRayMacros.cmake
@@ -12,17 +12,16 @@ macro(gammaray_add_plugin _target_name _desktop_file)
   if(NOT PLUGIN_INSTALL_DIR) # HACK for external plugins that don't set PLUGIN_INSTALL_DIR
     set(PLUGIN_INSTALL_DIR ${GAMMARAY_PLUGIN_INSTALL_DIR})
   endif()
-  set(_install_target_dir "${PLUGIN_INSTALL_DIR}/${GAMMARAY_PLUGIN_VERSION}/${GAMMARAY_PROBE_ABI}")
-  set(_build_target_dir "${PROJECT_BINARY_DIR}/${_install_target_dir}")
+  set(_build_target_dir "${PROJECT_BINARY_DIR}/${PROBE_PLUGIN_INSTALL_DIR}")
 
   add_library(${_target_name} MODULE ${ARGN})
   set_target_properties(${_target_name} PROPERTIES
     PREFIX ""
     LIBRARY_OUTPUT_DIRECTORY ${_build_target_dir}
   )
-  install(TARGETS ${_target_name} DESTINATION ${_install_target_dir})
+  install(TARGETS ${_target_name} DESTINATION ${PROBE_PLUGIN_INSTALL_DIR})
   if(NOT ${Qt5Core_FOUND})
-    install(FILES ${_desktop_file} DESTINATION ${_install_target_dir})
+    install(FILES ${_desktop_file} DESTINATION ${PROBE_PLUGIN_INSTALL_DIR})
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/${_desktop_file}" "${_build_target_dir}/${_desktop_file}")
   endif()
 endmacro()
