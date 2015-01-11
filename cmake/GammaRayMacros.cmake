1223eeb8163939bf5973399b6803bb103da5276f
Volker Krause <volker.krause@kdab.com>
2015-01-11 12:25:41 +0100

    Fix the install location fallback handling for external plugins.
    This got broken by the introduction of PROBE_PLUGIN_INSTALL_DIR.

--- cmake/GammaRayMacros.cmake
+++ cmake/GammaRayMacros.cmake
@@ -9,8 +9,8 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 
 macro(gammaray_add_plugin _target_name _desktop_file)
-  if(NOT PLUGIN_INSTALL_DIR) # HACK for external plugins that don't set PLUGIN_INSTALL_DIR
-    set(PLUGIN_INSTALL_DIR ${GAMMARAY_PLUGIN_INSTALL_DIR})
+  if(NOT PROBE_PLUGIN_INSTALL_DIR) # HACK for external plugins that don't set PLUGIN_INSTALL_DIR
+    set(PROBE_PLUGIN_INSTALL_DIR ${GAMMARAY_PROBE_PLUGIN_INSTALL_DIR})
   endif()
   set(_build_target_dir "${PROJECT_BINARY_DIR}/${PROBE_PLUGIN_INSTALL_DIR}")
 
