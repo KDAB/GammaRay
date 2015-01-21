92f4cf81c9715ff3578a176362d2368d7238db7d
Volker Krause <volker.krause@kdab.com>
2015-01-21 18:27:20 +0100

    Simple test case for the action deletion crash.

---
+++ tests/actiontest.cpp
@@ -0,0 +1,75 @@
+/*
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config-gammaray.h>
+
+#include <hooking/hooks.h>
+#include <hooking/probecreator.h>
+#include <core/probe.h>
+#include <common/paths.h>
+#include <common/objectbroker.h>
+
+#include <QtTest/qtest.h>
+
+#include <QAbstractItemModel>
+#include <QAction>
+#include <QObject>
+
+using namespace GammaRay;
+
+class ActionTest : public QObject
+{
+    Q_OBJECT
+private:
+    void createProbe()
+    {
+        Paths::setRelativeRootPath(GAMMARAY_INVERSE_BIN_DIR);
+        qputenv("GAMMARAY_ProbePath", Paths::currentProbePath().toUtf8());
+        Hooks::installHooks();
+        Probe::startupHookReceived();
+        new ProbeCreator(ProbeCreator::CreateOnly);
+        QTest::qWait(1); // event loop re-entry
+    }
+
+private slots:
+    void testActionCreationDeletion()
+    {
+        createProbe();
+
+        QAction *a1 = new QAction("Action 1", this);
+        QAction *a2 = new QAction("Action 2", this);
+        QTest::qWait(1); // event loop re-entry
+
+        auto *model = ObjectBroker::model("com.kdab.GammaRay.ActionModel");
+        QVERIFY(model);
+        QCOMPARE(model->rowCount(), 2);
+
+        delete a1;
+        QTest::qWait(1); // event loop re-entry
+
+        delete a2;
+        QTest::qWait(1); // event loop re-entry
+    }
+};
+
+QTEST_MAIN(ActionTest)
+
+#include "actiontest.moc"
