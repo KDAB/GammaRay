d188da2467996000aaa4b4bfd7fd4be205f083fc
Allen Winter <allen.winter@kdab.com>
2015-06-11 11:32:23 -0400

    add_license_blurb.sh - update for commerical license

--- add_license_blurb.sh
+++ add_license_blurb.sh
@@ -16,6 +16,11 @@ find "$@" -name '*.h' -o -name '*.cpp' -o -name '*.qml' | grep -v /3rdparty/ | g
   Copyright (C) $thisYear Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
   Author: $authorName <$authorEmail>
 
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 2 of the License, or
@@ -46,6 +51,11 @@ find "$@" -name 'CMakeLists.txt' | while read FILE; do
 #
 #  Copyright (C) $thisYear Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 #  Author: $authorName <$authorEmail>
+#
+#  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+#  accordance with GammaRay Commercial License Agreement provided with the Software.
+#
+#  Contact info@kdab.com if any conditions of this licensing are not clear to you.
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
