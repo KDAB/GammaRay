442aa11c976915762d2e3bb7b5133a5a50827cae
Volker Krause <volker.krause@kdab.com>
2015-06-26 14:19:42 +0200

    Update to the Qt 5.5 version of modeltest.
    Also move it to 3rdparty/.

--- tests/remotemodeltest.cpp
+++ tests/remotemodeltest.cpp
@@ -19,7 +19,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "modeltest.h"
+#include <3rdparty/qt/modeltest.h>
 
 #include <core/remote/remotemodelserver.h>
 #include <client/remotemodel.h>
