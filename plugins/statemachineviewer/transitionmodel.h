2e607547c57d05a21a8d75d419a30abae580fd1a
Volker Krause <volker.krause@kdab.com>
2015-06-30 15:31:40 +0200

    Add another batch of commercial license headers, almost done now.
    All contributions to these files were made by KDABians, people who have
    signed a CLA or trivial.

--- plugins/statemachineviewer/transitionmodel.h
+++ plugins/statemachineviewer/transitionmodel.h
@@ -4,6 +4,11 @@
   This file is part of GammaRay, the Qt application inspection and
   manipulation tool.
 
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
   Copyright (C) 2010-2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
   Author: Stephen Kelly <stephen.kelly@kdab.com>
 
