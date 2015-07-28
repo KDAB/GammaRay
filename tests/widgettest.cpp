8bcf84d76e2f74be9c5c339dc43cbc64c17b6072
Volker Krause <volker.krause@kdab.com>
2015-07-28 19:43:17 +0200

    Add unit test for widget tree reparenting.

---
+++ tests/widgettest.cpp
@@ -0,0 +1,102 @@
+/*
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config-gammaray.h>
+
+#include <hooking/hooks.h>
+#include <hooking/probecreator.h>
+#include <core/probe.h>
+#include <common/paths.h>
+#include <common/objectbroker.h>
+
+#include <3rdparty/qt/modeltest.h>
+
+#include <QtTest/qtest.h>
+
+#include <QAbstractItemModel>
+#include <QAction>
+#include <QObject>
+
+using namespace GammaRay;
+
+class WidgetTest : public QObject
+{
+    Q_OBJECT
+private:
+    void createProbe()
+    {
+        Paths::setRelativeRootPath(GAMMARAY_INVERSE_BIN_DIR);
+        qputenv("GAMMARAY_ProbePath", Paths::currentProbePath().toUtf8());
+        Hooks::installHooks();
+        Probe::startupHookReceived();
+        new ProbeCreator(ProbeCreator::CreateOnly);
+        QTest::qWait(1); // event loop re-entry
+    }
+
+private slots:
+    void testWidgetReparent()
+    {
+        createProbe();
+
+        auto *model = ObjectBroker::model("com.kdab.GammaRay.WidgetTree");
+        QVERIFY(model);
+        ModelTest modelTest(model);
+
+        auto w1 = new QWidget;
+        auto w2 = new QWidget;
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 2);
+
+        w2->setParent(w1);
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 1);
+
+        w2->setParent(w1);
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 1);
+
+        auto w3 = new QWidget;
+        w2->setParent(w3); // reparent without event loop reentry!
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 2);
+
+        delete w2;
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 2);
+
+        delete w1;
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 1);
+
+        delete w3;
+        QTest::qWait(1); // event loop re-entry
+        QCOMPARE(model->rowCount(), 0);
+    }
+};
+
+QTEST_MAIN(WidgetTest)
+
+#include "widgettest.moc"
