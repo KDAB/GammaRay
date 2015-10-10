d638d54d61d809258cee0ba1e2a855b8cb9599c9
Volker Krause <volker.krause@kdab.com>
2015-10-10 12:51:24 +0200

    Add Qt 5.4/5.5 QStandardPaths::StandardLocation enum values.

--- core/tools/standardpaths/standardpathsmodel.cpp
+++ core/tools/standardpaths/standardpathsmodel.cpp
@@ -55,7 +55,14 @@ static const standard_path_t standard_paths[] = {
   P(RuntimeLocation),
   P(ConfigLocation),
   P(DownloadLocation),
-  P(GenericCacheLocation)
+  P(GenericCacheLocation),
+#if QT_VERSION >= QT_VERSION_CHECK(5, 4, 0)
+  P(GenericConfigLocation),
+  P(AppDataLocation),
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
+  P(AppConfigLocation),
+#endif
 };
 
 #undef P
