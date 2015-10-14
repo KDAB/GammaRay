a485afa771b5a5f7dd63686f90c46815cb370a29
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-10-14 16:09:32 +0200

    Platform independant 32bits toolchain.

--- cmake/Toolchain-GCC-i686.cmake
+++ cmake/Toolchain-GCC-i686.cmake
@@ -4,10 +4,11 @@
 #  Copyright (c) 2013-2015 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 
 # Author: Volker Krause <volker.krause@kdab.com>
+# Author: Filipe Azevedo <filipe.azevedo@kdab.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 
-set(CMAKE_SYSTEM_NAME "Linux")
+#set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "i686")
 
 set(CMAKE_C_COMPILER_ARG1 "-m32")
