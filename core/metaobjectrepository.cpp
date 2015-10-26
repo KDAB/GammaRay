26059e5ed14e2f75b92c21b139444d9417d208a6
Volker Krause <volker.krause@kdab.com>
2015-10-26 15:14:45 +0100

    Compile with Qt <= 5.4 again.

--- core/metaobjectrepository.cpp
+++ core/metaobjectrepository.cpp
@@ -189,6 +189,9 @@ Q_DECLARE_METATYPE(QAbstractSocket::SocketState)
 Q_DECLARE_METATYPE(QNetworkProxy)
 #endif
 #endif
+#if QT_VERSION <= QT_VERSION_CHECK(5, 4, 0)
+Q_DECLARE_METATYPE(Qt::SortOrder)
+#endif
 
 void MetaObjectRepository::initIOTypes()
 {
