7da5624428db1ca4f32dd7d826d03353701a610c
Volker Krause <volker.krause@kdab.com>
2015-11-11 11:55:14 +0100

    Add a custom event to indicate whether a model is used or not.
    This will enable optimizations like the proxy model detaching we had in
    GammaRay 1.

---
+++ common/modelevent.cpp
@@ -0,0 +1,54 @@
+/*
+  modelevent.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "modelevent.h"
+
+using namespace GammaRay;
+
+ModelEvent::ModelEvent(bool modelUsed):
+    QEvent(eventType()),
+    m_used(modelUsed)
+{
+}
+
+ModelEvent::~ModelEvent()
+{
+}
+
+bool ModelEvent::used() const
+{
+    return m_used;
+}
+
+QEvent::Type ModelEvent::eventType()
+{
+    static int id = -1;
+    if (id < 0)
+        id = registerEventType();
+    return static_cast<QEvent::Type>(id);
+}
