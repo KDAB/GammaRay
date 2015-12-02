bc58de829f886de581937ce733df7d875b532139
Volker Krause <volker.krause@kdab.com>
2015-12-02 13:42:23 +0100

    Move MIPS preload check code to its own file.
    We do not have a CLA for it, so we need to cleanly separate it from the
    dual-licensed code.

--- launcher/injector/preloadcheck.cpp
+++ launcher/injector/preloadcheck.cpp
@@ -5,6 +5,11 @@
   Copyright (C) 2012-2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
   Author: Kevin Funk <kevin.funk@kdab.com>
 
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 2 of the License, or
@@ -100,80 +105,6 @@ bool PreloadCheck::test(const QString &fileName, const QString &symbol)
   return false;
 }
 
-#ifdef __mips__
-// The way to determine whether the call to function will go
-// through .got and lazy binding stub is:
-// - find the value of dynamic symbol index of the function with the command
-//   "readelf --dyn-syms"
-// - find the value of dynamic tag MIPS_GOTSYM with the command "readelf -d"
-// - if (dyn_sym_index >= MIPS_GOTSYM) then the function has entry in the global
-//   part of the .got, and the call will go through lazy binding stub and be
-//   resolved by dynamic linker.
-bool PreloadCheck::testMips(const QString &symbol, const QString &fileName)
-{
-  QProcess proc;
-  proc.setProcessChannelMode(QProcess::MergedChannels);
-  proc.start("readelf", QStringList() << "--dyn-syms" << "-d" << fileName, QIODevice::ReadOnly);
-
-  if (!proc.waitForFinished()) {
-    setErrorString(QObject::tr("Failed to run 'readelf' (binutils) binary: %1").
-                   arg(QString(proc.errorString())));
-    return false;
-  }
-
-  if (proc.exitCode() != 0) {
-    setErrorString(QObject::tr("Cannot read shared object: %1").arg(QString(proc.readAll())));
-    return false;
-  }
-
-  //Example line of dynamic symbol table on mips:
-  //3851: 001e66f4     8 FUNC    GLOBAL DEFAULT   11 qt_startup_hook
-  QRegExp rxSym("^(\\d+):\\s+(?:[^ ]+\\s+){6}([^ ]+)(?:.*)$");
-
-  //Example line of dynamic tag on mips:
-  //0x70000013 (MIPS_GOTSYM)                0xec3
-  QRegExp rxGot("^0x[0-9a-fA-F]+\\s+\\((.+)\\)\\s+(0x[0-9a-fA-F]+)(?:.*)$");
-
-  int dyn_sym_index = 0;
-  int mips_gotsym = 0;
-  bool foundDynSymbol = false;
-  bool foundGotTag = false;
-
-  while (proc.canReadLine()) {
-    const QString line = proc.readLine().trimmed();
-    QString currentMatch;
-    const QString tag = "MIPS_GOTSYM";
-
-    if (rxGot.exactMatch(line)) {
-      currentMatch = rxGot.cap(1);
-
-      if (currentMatch == tag) {
-        bool conversionOk = false;
-        int value = rxGot.cap(2).toInt(&conversionOk, 16);
-        if (conversionOk){
-          mips_gotsym = value;
-          foundGotTag = true;
-        }
-      }
-    }
-    else if (rxSym.exactMatch(line)) {
-      currentMatch = rxSym.cap(2);
-      if (currentMatch == symbol) {
-        dyn_sym_index = rxSym.cap(1).toInt();
-        foundDynSymbol = true;
-      }
-    }
-    if (foundGotTag && foundDynSymbol)
-      break;
-  }
-  if (foundGotTag && foundDynSymbol && (dyn_sym_index >= mips_gotsym)) {
-    return true;
-  }
-
-  return false;
-}
-#endif
-
 QString PreloadCheck::errorString() const
 {
   return m_errorString;
@@ -183,5 +114,9 @@ void PreloadCheck::setErrorString(const QString &err)
 {
   m_errorString = err;
 }
+
+#ifdef __mips__
+#include "preloadcheck_mips.cpp"
+#endif
 
 #endif
