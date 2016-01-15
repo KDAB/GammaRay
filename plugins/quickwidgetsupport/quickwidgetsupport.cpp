be6214e94484cfd22f8e1fadebe5f8c195fed7d0
Volker Krause <volker.krause@kdab.com>
2016-01-15 20:10:58 +0100

    Add basic type support for QQuickWidget.

---
+++ plugins/quickwidgetsupport/quickwidgetsupport.cpp
@@ -0,0 +1,62 @@
+/*
+  quickwidgetsupport.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "quickwidgetsupport.h"
+
+#include <core/metaobject.h>
+#include <core/metaobjectrepository.h>
+
+#include <common/metatypedeclarations.h>
+
+#include <QQmlContext>
+#include <QQmlEngine>
+#include <QQuickItem>
+
+using namespace GammaRay;
+
+QuickWidgetSupport::QuickWidgetSupport(ProbeInterface* probe, QObject* parent) :
+    QObject(parent)
+{
+    Q_UNUSED(probe);
+
+    MetaObject *mo = 0;
+    MO_ADD_METAOBJECT1(QQuickWidget, QWidget);
+    MO_ADD_PROPERTY_RO(QQuickWidget, QQmlEngine*, engine);
+    MO_ADD_PROPERTY_RO(QQuickWidget, QSurfaceFormat, format);
+    MO_ADD_PROPERTY_RO(QQuickWidget, QSize, initialSize);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
+    MO_ADD_PROPERTY_RO(QQuickWidget, QQuickWindow*, quickWindow);
+#endif
+    MO_ADD_PROPERTY_RO(QQuickWidget, QQmlContext*, rootContext);
+    MO_ADD_PROPERTY_RO(QQuickWidget, QQuickItem*, rootObject);
+}
+
+QString QuickWidgetSupportFactory::name() const
+{
+  return tr("Quick Widget Support");
+}
