f69cc0caa03ac56a43c9b0ecad60a032cbe12cf8
Allen Winter <allen.winter@kdab.com>
2016-01-27 17:58:09 -0500

    pluginmanager.cpp, probeabi.cpp - minor include clean

--- common/pluginmanager.cpp
+++ common/pluginmanager.cpp
@@ -26,7 +26,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "config-gammaray.h"
+#include <config-gammaray.h>
 #include "pluginmanager.h"
 #include "paths.h"
 
