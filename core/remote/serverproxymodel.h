KDAB/GammaRay

commit 91876d96f9835b724b9f1ba62483429a0ab4d8fe

Fix selection when using the contextual 'Shown In XXX'
Task-Id: KDEND-43

core/remote/serverproxymodel.h
@@ -87,10 +87,10 @@ public:
         }
     }
 
-    QModelIndexList match(const QModelIndex &start, int role, const QVariant &value, int hits, Qt::MatchFlags flags) const Q_DECL_OVERRIDE
+    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const Q_DECL_OVERRIDE
     {
         Model::used(this);
-        return BaseProxy::match(start, role, value, hits, flags);
+        return BaseProxy::index(row, column, parent);
     }
 
 protected:

core/probe.cpp
@@ -942,16 +942,13 @@ bool Probe::hasReliableObjectTracking() const
 
 void Probe::selectObject(QObject *object, const QPoint &pos)
 {
-  emit objectSelected(object, pos);
-
   const auto srcIdxs = m_toolModel->toolsForObject(object);
   selectTool(srcIdxs.value(0));
+  emit objectSelected(object, pos);
 }
 
 void Probe::selectObject(QObject* object, const QString toolId, const QPoint &pos)
 {
-  emit objectSelected(object, pos);
-
   const auto matches = m_toolModel->match(m_toolModel->index(0, 0), ToolModelRole::ToolId, toolId, 1, Qt::MatchExactly);
   if (matches.isEmpty()) {
     std::cerr << "Invalid tool id: " << qPrintable(toolId) << std::endl;
@@ -959,14 +956,14 @@ void Probe::selectObject(QObject* object, const QString toolId, const QPoint &po
   }
 
   selectTool(matches.first());
+  emit objectSelected(object, pos);
 }
 
 void Probe::selectObject(void *object, const QString &typeName)
 {
-  emit nonQObjectSelected(object, typeName);
-
   const auto srcIdxs = m_toolModel->toolsForObject(object, typeName);
   selectTool(srcIdxs.value(0));
+  emit nonQObjectSelected(object, typeName);
 }
 
 void Probe::selectTool(const QModelIndex& toolModelSourceIndex)

commit f2f4ed5fac3ad24da9792cf5a79ac736bea2480c

Fix shutdown crash/infinite loop regression introduced in 91876d96f.

core/remote/serverproxymodel.h
@@ -32,6 +32,7 @@
 #include <common/modelevent.h>
 
 #include <QCoreApplication>
+#include <QPointer>
 #include <QSortFilterProxyModel>
 #include <QVector>
 
@@ -114,7 +115,7 @@ protected:
 private:
     QVector<int> m_extraRoles;
     QVector<int> m_extraProxyRoles;
-    QAbstractItemModel *m_sourceModel;
+    QPointer<QAbstractItemModel> m_sourceModel;
     bool m_active;
 };
 