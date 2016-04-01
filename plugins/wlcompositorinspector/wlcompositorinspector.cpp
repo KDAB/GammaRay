15fa7d256e708cb529fb77727c604bbb8e254c8c
Giulio Camuffo <giulio.camuffo@kdab.com>
2016-04-01 10:27:41 +0300

    Show "Resources" in the resources' view header

--- plugins/wlcompositorinspector/wlcompositorinspector.cpp
+++ plugins/wlcompositorinspector/wlcompositorinspector.cpp
@@ -409,6 +409,14 @@ public:
         return map;
     }
 
+    QVariant headerData(int section, Qt::Orientation orientation, int) const override
+    {
+        if (orientation == Qt::Horizontal) {
+            return QStringLiteral("Resources");
+        }
+        return QString::number(section + 1);
+    }
+
     QVector<Resource *> m_resources;
     QSet<Resource *> m_allResources;
     ClientListener m_listener;
