589f8335d17c127a8384609a5d8c4459415a8183
Filipe Azevedo <filipe.azevedo@kdab.com>
2016-05-23 18:08:45 +0200

    Add multiline text editor.
    In some string properties it can be a lot of text (stylesheet,
    Qt3D shaders, ...), this was a bit a pain to edit them inline.
    
    The inline string editor is still the default, but it now have
    a toolbutton on the right to open a multiline editor.
    
    Task-Id: KDEND-51

---
+++ ui/propertyeditor/propertytexteditor.h
@@ -0,0 +1,70 @@
+/*
+  propertytexteditor.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2011-2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Filipe Azevedo <filipe.azevedo@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_PROPERTYTEXTEDITOR_H
+#define GAMMARAY_PROPERTYTEXTEDITOR_H
+
+#include <QDialog>
+#include <QLineEdit>
+
+namespace GammaRay {
+
+namespace Ui {
+  class PropertyTextEditorDialog;
+}
+
+/** Property editor dialog for texts. */
+class PropertyTextEditorDialog : public QDialog
+{
+  Q_OBJECT
+  public:
+    explicit PropertyTextEditorDialog(const QString &text, QWidget *parent = 0);
+    ~PropertyTextEditorDialog();
+
+    QString text() const;
+
+  protected:
+    QScopedPointer<Ui::PropertyTextEditorDialog> ui;
+};
+
+/** Property editor for texts. */
+class PropertyTextEditor : public QLineEdit
+{
+  Q_OBJECT
+  public:
+    explicit PropertyTextEditor(QWidget *parent = 0);
+
+    void save(const QString &text);
+
+  protected slots:
+    void edit();
+};
+
+}
+
+#endif // GAMMARAY_PROPERTYTEXTEDITOR_H
