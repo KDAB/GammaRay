c5b0dd288498dac521a10da19ef9c278567d0291
Volker Krause <volker.krause@kdab.com>
2016-05-28 16:51:39 +0200

    Add skeleton for the network plugin.

---
+++ plugins/network/networkwidget.h
@@ -0,0 +1,64 @@
+/*
+  networkwidget.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_NETWORKWIDGET_H
+#define GAMMARAY_NETWORKWIDGET_H
+
+#include <ui/tooluifactory.h>
+
+#include <QScopedPointer>
+#include <QWidget>
+
+namespace GammaRay {
+
+namespace Ui
+{
+class NetworkWidget;
+}
+
+class NetworkWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit NetworkWidget(QWidget *parent = Q_NULLPTR);
+    ~NetworkWidget();
+
+private:
+    QScopedPointer<Ui::NetworkWidget> ui;
+};
+
+class NetworkWidgetFactory : public QObject, public StandardToolUiFactory<NetworkWidget>
+{
+  Q_OBJECT
+  Q_INTERFACES(GammaRay::ToolUiFactory)
+  Q_PLUGIN_METADATA(IID "com.kdab.GammaRay.ToolUiFactory" FILE "gammaray_network.json")
+};
+
+}
+
+#endif // GAMMARAY_NETWORKWIDGET_H
