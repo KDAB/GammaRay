8e3da7fd23d8c68d2e5fc03348c7a598961043e2
Volker Krause <volker.krause@kdab.com>
2016-06-05 15:19:01 +0200

    Add unit test for ObjectInstance.

---
+++ tests/objectinstancetest.cpp
@@ -0,0 +1,117 @@
+/*
+  objectinstancetest.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <core/objectinstance.h>
+
+#include <QDebug>
+#include <QtTest/qtest.h>
+
+Q_DECLARE_METATYPE(QDateTime*)
+
+struct CustomType {};
+Q_DECLARE_METATYPE(CustomType)
+Q_DECLARE_METATYPE(CustomType*)
+
+using namespace GammaRay;
+
+class ObjectInstanceTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void testQtObject()
+    {
+        QObject obj;
+
+        ObjectInstance oi(QVariant::fromValue(&obj));
+        QCOMPARE(oi.type(), ObjectInstance::QtObject);
+        QCOMPARE(oi.typeName(), QByteArray("QObject"));
+        QCOMPARE(oi.object(), &obj);
+        QCOMPARE(oi.qtObject(), &obj);
+        QCOMPARE(oi.metaObject(), &QObject::staticMetaObject);
+
+        auto oi2 = oi;
+        QCOMPARE(oi2.type(), ObjectInstance::QtObject);
+        QCOMPARE(oi.object(), oi2.object());
+    }
+
+    void testMetaObjectVariantPointer()
+    {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
+        QDateTime dt;
+
+        ObjectInstance oi(QVariant::fromValue(&dt));
+        QCOMPARE(oi.type(), ObjectInstance::Object);
+        QCOMPARE(oi.typeName(), QByteArray("QDateTime*"));
+        QVERIFY(oi.object());
+
+        auto oi2 = oi;
+        QCOMPARE(oi2.type(), ObjectInstance::Object);
+        QCOMPARE(oi.object(), oi2.object());
+#endif
+    }
+
+    void testMetaObjectVariantValue()
+    {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
+        QDateTime dt;
+
+        ObjectInstance oi(QVariant::fromValue(dt));
+        QCOMPARE(oi.type(), ObjectInstance::Object);
+        QCOMPARE(oi.typeName(), QByteArray("QDateTime"));
+        QVERIFY(oi.object());
+
+        auto oi2 = oi;
+        QCOMPARE(oi2.type(), ObjectInstance::Object);
+        QVERIFY(oi2.object());
+#endif
+    }
+
+    void testUnknownVariantValue()
+    {
+        CustomType t;
+
+        ObjectInstance oi(QVariant::fromValue(t));
+        QCOMPARE(oi.type(), ObjectInstance::QtVariant);
+        QCOMPARE(oi.typeName(), QByteArray("CustomType"));
+    }
+
+    void testUnknownVariantPointer()
+    {
+        CustomType t;
+
+        ObjectInstance oi(QVariant::fromValue(&t));
+        QCOMPARE(oi.type(), ObjectInstance::QtVariant);
+        QCOMPARE(oi.typeName(), QByteArray("CustomType*"));
+    }
+
+};
+
+QTEST_MAIN(ObjectInstanceTest)
+
+#include "objectinstancetest.moc"
