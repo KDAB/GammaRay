691888c4c11e9d5b5ef0f5ac47e96f4729530930
Volker Krause <volker.krause@kdab.com>
2016-08-04 09:42:22 +0200

    Don't leak model test results

--- core/tools/modelinspector/modeltester.h
+++ core/tools/modelinspector/modeltester.h
@@ -44,6 +44,7 @@ class ModelTester : public QObject
     Q_OBJECT
 public:
     explicit ModelTester(QObject *parent = 0);
+    ~ModelTester();
 
     void failure(QAbstractItemModel *model, const char *file, int line, const char *message);
 
