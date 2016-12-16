fc559058a61655f14796f1ae7cd17b8114493ad1
Filipe Azevedo <filipe.azevedo@kdab.com>
2016-12-16 14:10:33 +0100

    Add a way to clear frame data
    Most of the time the frame data introduce data of a given item
    selection.
    Clearing those data allow quick way to erase the extra decoration
    when the frame does not change but a selection changed to null.
    
    Task-Id: KDEND-108

--- ui/remoteviewwidget.h
+++ ui/remoteviewwidget.h
@@ -192,6 +192,7 @@ private slots:
     void pickElementId(const QModelIndex &index);
     void elementsAtReceived(const GammaRay::ObjectIds &ids, int bestCandidate);
     void frameUpdated(const GammaRay::RemoteViewFrame &frame);
+    void clearFrameData();
 
 private:
     RemoteViewFrame m_frame;
