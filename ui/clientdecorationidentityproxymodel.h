8acfec48065977741ff8fb6cdabafd84f7334208
Filipe Azevedo <filipe.azevedo@kdab.com>
2017-04-14 14:41:38 +0200

    Introduce ClientDecorationIdentityProxyModel
    This introduce a ObjectModel::DecorationIdRole role that is mapped to a
    class icon file path which can be retrieved both server and client side.
    This allow server models to only need to set their
    ObjectModel::DecorationIdRole data and the client will take care of
    displaying the correct mapped icon, resulting in less network traffic.
    
    Client side, you can wrap your model into a
    ClientDecorationIdentityProxyModel instance to get free handling of the
    model decorations.
    
    Task-Id: KDEND-151

---
+++ ui/clientdecorationidentityproxymodel.h
@@ -0,0 +1,54 @@
+/*
+  clientdecorationidentityproxymodel.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  Copyright (C) 2010-2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Filipe Azevedo <filipe.azevedo@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_CLIENTDECORATIONIDENTITYPROXYMODEL_H
+#define GAMMARAY_CLIENTDECORATIONIDENTITYPROXYMODEL_H
+
+#include "gammaray_ui_export.h"
+
+#include <QIdentityProxyModel>
+#include <QPointer>
+
+namespace GammaRay {
+class ClassesIconsRepository;
+
+class GAMMARAY_UI_EXPORT ClientDecorationIdentityProxyModel : public QIdentityProxyModel
+{
+    Q_OBJECT
+
+public:
+    explicit ClientDecorationIdentityProxyModel(QObject *parent = nullptr);
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+    QPointer<ClassesIconsRepository> m_classesIconsRepository;
+};
+}
+
+#endif // GAMMARAY_CLIENTDECORATIONIDENTITYPROXYMODEL_H
