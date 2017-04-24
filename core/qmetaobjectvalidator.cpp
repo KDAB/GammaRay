2248b18cb90ed4212590e045a4ec4aa9fc589fd4
Volker Krause <volker.krause@kdab.com>
2017-04-24 11:33:43 +0200

    Properly include config.h
    Found by krazy.

--- core/qmetaobjectvalidator.cpp
+++ core/qmetaobjectvalidator.cpp
@@ -26,7 +26,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "config-gammaray.h"
+#include <config-gammaray.h>
 
 #include "qmetaobjectvalidator.h"
 #include "execution.h"
