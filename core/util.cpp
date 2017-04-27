7c3284f21d0610ae46301cefcc7c8ffebcfd4175
Volker Krause <volker.krause@kdab.com>
2017-04-27 11:13:45 +0200

    Fix format warning

--- core/util.cpp
+++ core/util.cpp
@@ -78,7 +78,7 @@ QString Util::shortDisplayString(const QObject *object)
 QString Util::addressToString(const void *p)
 {
     char buf[20];
-    sprintf(buf, "0x%x\0", p);
+    snprintf(buf, sizeof(buf), "0x%llx", reinterpret_cast<quint64>(p));
     return QString::fromLatin1(buf);
 }
 
