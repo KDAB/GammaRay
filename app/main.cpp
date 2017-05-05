783e54db62e1bc66612f8c7da602c0d9d2c59447
Volker Krause <volker.krause@kdab.com>
2017-05-05 13:38:52 +0200

    Set the application version, so version telemetry actually works

--- app/main.cpp
+++ app/main.cpp
@@ -27,6 +27,7 @@
 */
 
 #include <config-gammaray.h>
+#include <config-gammaray-version.h>
 
 #include <common/paths.h>
 
@@ -116,6 +117,7 @@ int main(int argc, char **argv)
     QCoreApplication::setOrganizationName("KDAB");
     QCoreApplication::setOrganizationDomain("kdab.com");
     QCoreApplication::setApplicationName("GammaRay");
+    QCoreApplication::setApplicationVersion(GAMMARAY_COMPACT_VERSION_STRING);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
--- client/main.cpp
+++ client/main.cpp
@@ -27,6 +27,7 @@
 */
 
 #include <config-gammaray.h>
+#include <config-gammaray-version.h>
 
 #include "client.h"
 #include "clientconnectionmanager.h"
@@ -45,6 +46,7 @@ int main(int argc, char **argv)
     QCoreApplication::setOrganizationName("KDAB");
     QCoreApplication::setOrganizationDomain("kdab.com");
     QCoreApplication::setApplicationName("GammaRay");
+    QCoreApplication::setApplicationVersion(GAMMARAY_COMPACT_VERSION_STRING);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
