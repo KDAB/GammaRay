6d7e26af20512076ccfd7113d736a21a0815e73a
Kevin Funk <kevin.funk@kdab.com>
2017-05-23 17:08:44 +0200

    Make compiler under MSVC2010 again

--- core/util.cpp
+++ core/util.cpp
@@ -137,7 +137,7 @@ static QString stringifyProperty(const QObject *obj, const QString &propName)
 
 struct IconCacheEntry
 {
-    IconCacheEntry(const QByteArray &className_ = {})
+    IconCacheEntry(const QByteArray &className_ = QByteArray())
         : className(className_)
         , defaultIcon(-1)
     {}
