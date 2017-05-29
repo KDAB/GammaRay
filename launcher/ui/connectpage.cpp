f68844bb0290f50a557d6201dc93d540f96cb502
Christoph Sterz <christoph.sterz@kdab.com>
2017-05-29 12:01:50 +0200

    LauncherUI: Smart HostAddress Input
    This commit should ease the input of urls in the Launcher UI:
    Therefore the LineEdit in the UI is now:
      1. Versatile in accepting Urls (Localsockets, IPv4, all IPv6 formats, IPv4 embedded in IPv6)
      2. Interactive (changing textcolor to red if address not accepted so far)
      2. Forgiving for recoverable mistakes (forgetting 'local://' or 'tcp://' or the default port)
      3. Suggestive (warning when default port was chosen automatically, alert when file is not socket)
    
    Here are some representatives of parsed URLs:
    %On UNIX only
    /tmp/real_socket
    
    %On All Platforms
    192.168.150.139 (implicit port warning)
    192.168.150.139:1414
    
    tcp://192.168.150.139 (implicit port warning)
    tcp://192.168.150.139:1414
    
    ::1
    fe80::9c0e:f1f4:d51d:a557 (implicit port warning)
    fe80::9c0e:f1f4:d51d:a557%enp0s31f6 (implicit port warning)
    fe80::9c0e:f1f4:d51d:a557%enp0s31f6:11732
    tcp://fe80::9c0e:f1f4:d51d:a557%enp0s31f6:11732
    
    [fe80::9c0e:f1f4:d51d:a557] (implicit port warning)
    [fe80::9c0e:f1f4:d51d:a557]:1414
    
    ::ffff:192.168.15.2 (implicit port warning)
    [::ffff:192.168.15.2]:1414

--- launcher/ui/connectpage.cpp
+++ launcher/ui/connectpage.cpp
@@ -34,19 +34,34 @@
 
 #include <common/endpoint.h>
 
+#include <QAction>
+#include <QFileInfo>
 #include <QProcess>
 #include <QSettings>
+
+#include <qplatformdefs.h>
+
+#ifdef Q_OS_LINUX
+#include <sys/stat.h>
+#endif
 
 using namespace GammaRay;
 
 ConnectPage::ConnectPage(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ConnectPage)
+    , m_localPrefix("local://")
+    , m_tcpPrefix("tcp://")
 {
+    m_implicitPortWarningSign = new QAction(QIcon(":/launcher/warning.png"), "No port given, will use 11732", this);
+    m_fileIsNotASocketWarning = new QAction(qApp->style()->standardIcon(QStyle::SP_MessageBoxCritical)
+                                            , "File is not a socket"
+                                            , this);
+
     ui->setupUi(this);
 
+    connect(ui->host, SIGNAL(textChanged(QString)), SLOT(validateHostAddress(const QString&)));
     connect(ui->host, SIGNAL(textChanged(QString)), SIGNAL(updateButtonState()));
-    connect(ui->port, SIGNAL(valueChanged(int)), SIGNAL(updateButtonState()));
 
     auto *model = new NetworkDiscoveryModel(this);
     ui->instanceView->setModel(model);
@@ -55,9 +70,130 @@ ConnectPage::ConnectPage(QWidget *parent)
     connect(ui->instanceView, SIGNAL(activated(QModelIndex)), SIGNAL(activate()));
 
     QSettings settings;
-    ui->host->setText(settings.value(QStringLiteral("Connect/Host"), QString()).toString());
-    ui->port->setValue(settings.value(QStringLiteral("Connect/Port"),
-                                      Endpoint::defaultPort()).toInt());
+    ui->host->setText(settings.value(QStringLiteral("Connect/Url"), QString()).toString());
+}
+
+void ConnectPage::validateHostAddress(const QString &address)
+{
+    QString stillToParse = address;
+    m_valid = false;
+    bool correctSoFar = false;
+    m_currentUrl.clear();
+
+    // Initially, set the text to red
+    QPalette palette;
+    palette.setColor(QPalette::Text,Qt::red);
+    ui->host->setPalette(palette);
+    ui->host->removeAction(m_implicitPortWarningSign);
+    ui->host->removeAction(m_fileIsNotASocketWarning);
+
+    handleLocalAddress(stillToParse, correctSoFar);
+    handleIPAddress(stillToParse, correctSoFar);
+
+    // if we came down here and theres nothing more to parse, we are good
+    // set text back to black again
+    if (correctSoFar && stillToParse.isEmpty()) {
+        m_valid = true;
+        palette.setColor(QPalette::Text,Qt::black);
+        ui->host->setPalette(palette);
+    }
+}
+
+void ConnectPage::handleLocalAddress(QString &stillToParse, bool &correctSoFar)
+{
+#ifdef Q_OS_UNIX
+    if (stillToParse.startsWith(m_localPrefix))
+        stillToParse.remove(0, m_localPrefix.size()-1); //dont remove second slash
+
+    // Its also okay, if only a path to an existing file is given
+    QFileInfo localSocketFile(stillToParse);
+    if (localSocketFile.exists() && !localSocketFile.isDir() && !localSocketFile.isSymLink()) {
+        QT_STATBUF statbuf;
+        QT_STAT(QFile::encodeName(localSocketFile.filePath()), &statbuf);
+        if(!S_ISSOCK(statbuf.st_mode)) {
+            ui->host->addAction(m_fileIsNotASocketWarning, QLineEdit::TrailingPosition);
+        } else {
+            stillToParse = "";
+            correctSoFar = true;
+            m_currentUrl.setScheme("local");
+            m_currentUrl.setPath(localSocketFile.filePath());
+        }
+    }
+#endif
+}
+
+void ConnectPage::handleIPAddress(QString &stillToParse, bool &correctSoFar)
+{
+    // handle tcp prefix
+    if (stillToParse.startsWith(m_tcpPrefix))
+        stillToParse.remove(0, m_tcpPrefix.size());
+
+    // Speculate on the address format
+    const auto possibleIPv4Address = QHostAddress(stillToParse.split(":").first());
+
+    QHostAddress possibleIPv6Address;
+    if (!stillToParse.contains("%"))
+        possibleIPv6Address = QHostAddress(stillToParse);
+
+    QHostAddress possibleIPv6BracketAddress;
+    QRegularExpression bracketFormat("^\\[([0-9,a-f,\\:,\\.]*)\\].*$");
+    const auto bracketFormatMatch = bracketFormat.match(stillToParse);
+    if (bracketFormatMatch.hasMatch())
+        possibleIPv6BracketAddress = QHostAddress(bracketFormatMatch.captured(1));
+
+    QHostAddress possibleIPv6InterfaceAddress;
+    QRegularExpression interfaceFormat("^([^\\%]*)(\\%[^\\:]+)(:[0-9]+)?$");
+    const auto interfaceFormatMatch = interfaceFormat.match(stillToParse);
+    if (interfaceFormatMatch.hasMatch())
+        possibleIPv6InterfaceAddress = QHostAddress(interfaceFormatMatch.captured(1));
+
+    const auto skipPort = true;
+    if (!possibleIPv4Address.isNull())
+        handleAddressAndPort(stillToParse, correctSoFar, possibleIPv4Address.toString());
+    if (!possibleIPv6Address.isNull())
+        handleAddressAndPort(stillToParse, correctSoFar, possibleIPv6Address.toString(), skipPort);
+    if (!possibleIPv6BracketAddress.isNull())
+        handleAddressAndPort(stillToParse, correctSoFar, "[" + possibleIPv6BracketAddress.toString() + "]");
+    if (!possibleIPv6InterfaceAddress.isNull()){
+        stillToParse.replace(interfaceFormatMatch.captured(2), "");
+        handleAddressAndPort(stillToParse, correctSoFar, possibleIPv6InterfaceAddress.toString());
+    }
+}
+
+void ConnectPage::handleAddressAndPort(QString &stillToParse, bool &correctSoFar, const QString &possibleAddress, bool skipPort)
+{
+    stillToParse.replace(possibleAddress, "");
+    if (stillToParse.isEmpty()) {
+        correctSoFar = true;
+        m_currentUrl.setScheme("tcp");
+        m_currentUrl.setHost(possibleAddress);
+        m_currentUrl.setPort(Endpoint::defaultPort());
+        showStandardPortAssumedWarning();
+    } else if(!skipPort) {
+        m_currentUrl.setScheme("tcp");
+        m_currentUrl.setHost(possibleAddress);
+        handlePortString(stillToParse, correctSoFar);
+    }
+}
+
+void ConnectPage::handlePortString(QString &stillToParse, bool &correctSoFar)
+{
+    QRegularExpression r("\\:[0-9]{1,5}");
+    const auto portMatch = r.match(stillToParse);
+    if (portMatch.hasMatch()) {
+        auto portString = portMatch.captured(0);
+        stillToParse = stillToParse.replace(portString, "");
+        auto portNumber = portString.replace(":","").toInt();
+        if (portNumber <= 65535){
+            m_currentUrl.setPort(portNumber);
+            correctSoFar = true;
+        }
+    }
+}
+
+void ConnectPage::showStandardPortAssumedWarning()
+{
+    ui->host->addAction(m_implicitPortWarningSign, QLineEdit::TrailingPosition);
 }
 
 ConnectPage::~ConnectPage()
@@ -66,23 +202,18 @@ ConnectPage::~ConnectPage()
 
 bool ConnectPage::isValid() const
 {
-    return !ui->host->text().isEmpty();
+    return m_valid;
 }
 
 void ConnectPage::launchClient()
 {
-    QUrl url;
-    url.setScheme(QStringLiteral("tcp"));
-    url.setHost(ui->host->text());
-    url.setPort(ui->port->value());
-    ClientLauncher::launchDetached(url);
+    ClientLauncher::launchDetached(m_currentUrl);
 }
 
 void ConnectPage::writeSettings()
 {
     QSettings settings;
-    settings.setValue(QStringLiteral("Connect/Host"), ui->host->text());
-    settings.setValue(QStringLiteral("Connect/Port"), ui->port->value());
+    settings.setValue(QStringLiteral("Connect/Url"), ui->host->text());
 }
 
 void ConnectPage::instanceSelected()
@@ -91,6 +222,5 @@ void ConnectPage::instanceSelected()
     if (rows.size() != 1)
         return;
 
-    ui->host->setText(rows.first().data(NetworkDiscoveryModel::HostNameRole).toString());
-    ui->port->setValue(rows.first().data(NetworkDiscoveryModel::PortRole).toInt());
+    ui->host->setText(rows.first().data(NetworkDiscoveryModel::UrlStringRole).toString());
 }
