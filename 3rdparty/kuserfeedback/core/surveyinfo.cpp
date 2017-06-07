63456f2618081a8d15eaa840f7dde799abb5bdd5
Allen Winter <allen.winter@kdab.com>
2017-06-07 11:10:51 -0400

    kuserfeedback/core/surveyinfo.cpp - fix compile error with MSVC

--- 3rdparty/kuserfeedback/core/surveyinfo.cpp
+++ 3rdparty/kuserfeedback/core/surveyinfo.cpp
@@ -93,11 +93,9 @@ void SurveyInfo::setTarget(const QString &target)
 SurveyInfo SurveyInfo::fromJson(const QJsonObject& obj)
 {
     SurveyInfo s;
-#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
     s.setUuid(obj.value(QLatin1String("uuid")).toString());
     s.setUrl(QUrl(obj.value(QLatin1String("url")).toString()));
     s.setTarget(obj.value(QLatin1String("target")).toString());
-#endif
     return s;
 }
 #endif
