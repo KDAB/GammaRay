9feb009449c07eeba0480d7c2a5083b767a996fb
Filipe Azevedo <filipe.azevedo@kdab.com>
2017-06-19 13:05:28 +0200

    Custom signal proxy model to be able to sort monitored column
    Task-Id: KDEND-99

---
+++ plugins/signalmonitor/signalhistoryproxy.h
@@ -0,0 +1,51 @@
+/*
+  signalhistoryproxy.h
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2013-2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Filipe Azevedo <filipe.azevedo@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GAMMARAY_SIGNALHISTORYPROXY_H
+#define GAMMARAY_SIGNALHISTORYPROXY_H
+
+#include <core/remote/serverproxymodel.h>
+
+QT_BEGIN_NAMESPACE
+class QSortFilterProxyModel;
+QT_END_NAMESPACE
+
+namespace GammaRay {
+typedef ServerProxyModel<QSortFilterProxyModel> SignalHistoryBaseProxy;
+
+class SignalHistoryProxy : public SignalHistoryBaseProxy
+{
+    Q_OBJECT
+public:
+    explicit SignalHistoryProxy(QObject *parent = nullptr);
+
+    void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
+};
+} // namespace GammaRay
+
+#endif // GAMMARAY_SIGNALHISTORYPROXY_H
