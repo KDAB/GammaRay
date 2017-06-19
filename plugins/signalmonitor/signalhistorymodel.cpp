4bb6ec9373df8dfcbb9b36d29990315d43bd9fa2
Filipe Azevedo <filipe.azevedo@kdab.com>
2017-06-19 14:38:35 +0200

    Add missing QCocoaEventDispatcher check
    Task-Id: KDEND-99

--- plugins/signalmonitor/signalhistorymodel.cpp
+++ plugins/signalmonitor/signalhistorymodel.cpp
@@ -219,6 +219,7 @@ void SignalHistoryModel::onObjectAdded(QObject *object)
     // blacklist event dispatchers
     if (qstrncmp(object->metaObject()->className(), "QPAEventDispatcher", 18) == 0
         || qstrncmp(object->metaObject()->className(), "QGuiEventDispatcher", 19) == 0
+        || qstrncmp(object->metaObject()->className(), "QCocoaEventDispatcher", 21) == 0
         || qstrncmp(object->metaObject()->className(), "QEventDispatcher", 16) == 0)
         return;
 
