1abcf770aa114d3255c0bdc66e6e698847f12474
Volker Krause <volker.krause@kdab.com>
2017-07-10 11:44:39 +0200

    Add test rig for the QtQuick material extension

---
+++ tests/manual/shadereffect.qml
@@ -0,0 +1,71 @@
+/*
+  shadereffect.qml
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import QtQuick 2.0
+
+import QtQuick 2.0
+
+Image {
+    id: root
+    source: "lsd.png"
+
+    Text {
+        anchors.centerIn: parent
+        text: "Drugs are bad, mkay?"
+    }
+
+    ShaderEffectSource {
+        id: effectSource
+        anchors.fill: parent
+        sourceItem: root
+        recursive: true
+        live: false
+    }
+
+    ShaderEffect {
+        anchors.fill: parent
+        property variant src: effectSource
+        vertexShader: "
+            uniform highp mat4 qt_Matrix;
+            attribute highp vec4 qt_Vertex;
+            attribute highp vec2 qt_MultiTexCoord0;
+            varying highp vec2 coord;
+            void main() {
+                coord = qt_MultiTexCoord0;
+                gl_Position = qt_Matrix * qt_Vertex;
+            }"
+        fragmentShader: "
+            varying highp vec2 coord;
+            uniform sampler2D src;
+            uniform lowp float qt_Opacity;
+            void main() {
+                lowp vec4 tex = texture2D(src, coord);
+                gl_FragColor = vec4(vec3(dot(tex.rgb, vec3(0.344, 0.5, 0.156))), tex.a) * qt_Opacity;
+            }"
+    }
+}
---
+++ tests/quickmaterialtest.cpp
@@ -0,0 +1,219 @@
+/*
+  quickmaterialtest.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Volker Krause <volker.krause@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config-gammaray.h>
+
+#include <plugins/quickinspector/materialextension/materialextensioninterface.h>
+#include <probe/hooks.h>
+#include <probe/probecreator.h>
+#include <core/probe.h>
+#include <core/propertycontroller.h>
+#include <common/paths.h>
+#include <common/objectbroker.h>
+
+#include <QAbstractItemModel>
+#include <QQuickItem>
+#include <QQuickView>
+#include <QSignalSpy>
+#include <QtTest/qtest.h>
+
+#include <private/qquickitem_p.h>
+
+#include <memory>
+
+using namespace GammaRay;
+
+static QSGGeometryNode *findGeometryNode(QSGNode *node)
+{
+    while(node) {
+        if (node->type() == QSGNode::GeometryNodeType)
+            return static_cast<QSGGeometryNode*>(node);
+        if (node->childCount() == 0)
+            return nullptr;
+        if (node->childCount() > 1 && node->firstChild()->type() != QSGNode::GeometryNodeType)
+            return nullptr;
+        node = node->firstChild();
+    }
+
+    return nullptr;
+}
+
+class QuickMaterialTest : public QObject
+{
+    Q_OBJECT
+private:
+    void createProbe()
+    {
+        Paths::setRelativeRootPath(GAMMARAY_INVERSE_BIN_DIR);
+        qputenv("GAMMARAY_ProbePath", Paths::probePath(GAMMARAY_PROBE_ABI).toUtf8());
+        qputenv("GAMMARAY_ServerAddress", GAMMARAY_DEFAULT_LOCAL_TCP_URL);
+        Hooks::installHooks();
+        Probe::startupHookReceived();
+        new ProbeCreator(ProbeCreator::Create);
+        QTest::qWait(1); // event loop re-entry
+    }
+
+    bool showSource(const QString &sourceFile)
+    {
+        QSignalSpy renderSpy(m_view.get(), SIGNAL(frameSwapped()));
+        Q_ASSERT(renderSpy.isValid());
+
+        m_view->setSource(QUrl(sourceFile));
+        m_view->show();
+        auto exposed = QTest::qWaitForWindowExposed(m_view.get());
+        if (!exposed) {
+            qWarning() << "Unable to expose window, probably running tests on a headless system - ignoring all following render failures.";
+            return false;
+        }
+
+        // wait at least two frames so we have the final window size with all render loop/driver combinations...
+        QTest::qWait(20);
+        renderSpy.wait();
+        m_view->update();
+        return renderSpy.wait();
+    }
+
+    std::unique_ptr<QQuickView> m_view;
+
+private slots:
+    void init()
+    {
+        createProbe();
+
+        m_view.reset(new QQuickView);
+        m_view->setResizeMode(QQuickView::SizeViewToRootObject);
+        QTest::qWait(1); // event loop re-entry
+    }
+
+    void testStaticShader()
+    {
+        if (!showSource("qrc:/manual/shadereffect.qml"))
+            return;
+
+        auto iface = ObjectBroker::object<MaterialExtensionInterface*>("com.kdab.GammaRay.QuickSceneGraph.material");
+        QVERIFY(iface);
+        QSignalSpy shaderSpy(iface, SIGNAL(gotShader(QString)));
+        QVERIFY(shaderSpy.isValid());
+
+        auto propModel = ObjectBroker::model("com.kdab.GammaRay.QuickSceneGraph.materialPropertyModel");
+        QVERIFY(propModel);
+        QCOMPARE(propModel->rowCount(), 0);
+
+        auto shaderModel = ObjectBroker::model("com.kdab.GammaRay.QuickSceneGraph.shaderModel");
+        QVERIFY(shaderModel);
+        QCOMPARE(shaderModel->rowCount(), 0);
+
+        auto imageItem = m_view->rootObject();
+        QVERIFY(imageItem);
+        QCOMPARE(imageItem->metaObject()->className(), "QQuickImage");
+        Probe::instance()->selectObject(imageItem, QPoint());
+
+        auto controller = ObjectBroker::object<PropertyController*>("com.kdab.GammaRay.QuickSceneGraph.controller");
+        QVERIFY(controller);
+
+        auto priv = QQuickItemPrivate::get(imageItem);
+        QVERIFY(priv->itemNodeInstance);
+        auto geometryNode = findGeometryNode(priv->itemNodeInstance);
+        QVERIFY(geometryNode);
+        controller->setObject(geometryNode, "QSGGeometryNode");
+
+        QVERIFY(propModel->rowCount() > 1);
+        QCOMPARE(shaderModel->rowCount(), 2);
+
+        iface->getShader(shaderModel->index(0,0).data(Qt::DisplayRole).toString());
+        QCOMPARE(shaderSpy.size(), 1);
+        QVERIFY(!shaderSpy.at(0).at(0).toString().isEmpty());
+        shaderSpy.clear();
+
+        iface->getShader(shaderModel->index(1,0).data(Qt::DisplayRole).toString());
+        QCOMPARE(shaderSpy.size(), 1);
+        QVERIFY(!shaderSpy.at(0).at(0).toString().isEmpty());
+
+        controller->setObject(nullptr, QString());
+    }
+
+    void testDynamicShader()
+    {
+        if (!showSource("qrc:/manual/shadereffect.qml"))
+            return;
+
+        auto iface = ObjectBroker::object<MaterialExtensionInterface*>("com.kdab.GammaRay.QuickSceneGraph.material");
+        QVERIFY(iface);
+        QSignalSpy shaderSpy(iface, SIGNAL(gotShader(QString)));
+        QVERIFY(shaderSpy.isValid());
+
+        auto propModel = ObjectBroker::model("com.kdab.GammaRay.QuickSceneGraph.materialPropertyModel");
+        QVERIFY(propModel);
+        QCOMPARE(propModel->rowCount(), 0);
+
+        auto shaderModel = ObjectBroker::model("com.kdab.GammaRay.QuickSceneGraph.shaderModel");
+        QVERIFY(shaderModel);
+        QCOMPARE(shaderModel->rowCount(), 0);
+
+        QQuickItem *effectItem = nullptr;
+        foreach (auto item, qFindChildren<QQuickItem*>(m_view->rootObject(), QString())) {
+            if (item->inherits("QQuickShaderEffect"))
+                effectItem = item;
+        }
+        QVERIFY(effectItem);
+        Probe::instance()->selectObject(effectItem, QPoint());
+
+        auto controller = ObjectBroker::object<PropertyController*>("com.kdab.GammaRay.QuickSceneGraph.controller");
+        QVERIFY(controller);
+
+        auto priv = QQuickItemPrivate::get(effectItem);
+        QVERIFY(priv->itemNodeInstance);
+        auto geometryNode = findGeometryNode(priv->itemNodeInstance);
+        QVERIFY(geometryNode);
+        controller->setObject(geometryNode, "QSGGeometryNode");
+
+        QEXPECT_FAIL("", "not yet implemented", Abort);
+        QVERIFY(propModel->rowCount() > 1);
+        QCOMPARE(shaderModel->rowCount(), 2);
+
+        iface->getShader(shaderModel->index(0,0).data(Qt::DisplayRole).toString());
+        QCOMPARE(shaderSpy.size(), 1);
+        QVERIFY(!shaderSpy.at(0).at(0).toString().isEmpty());
+        shaderSpy.clear();
+
+        iface->getShader(shaderModel->index(1,0).data(Qt::DisplayRole).toString());
+        QCOMPARE(shaderSpy.size(), 1);
+        QVERIFY(!shaderSpy.at(0).at(0).toString().isEmpty());
+
+        controller->setObject(nullptr, QString());
+    }
+
+    void cleanup()
+    {
+        m_view.reset();
+    }
+};
+
+QTEST_MAIN(QuickMaterialTest)
+
+#include "quickmaterialtest.moc"
