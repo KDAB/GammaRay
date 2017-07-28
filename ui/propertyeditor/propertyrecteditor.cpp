9e7c9287ca1b7b02cec424d7287ae4255371db35
Allen Winter <allen.winter@kdab.com>
2017-07-28 16:39:45 -0400

    ui/propertyeditor/propertyrecteditor - add license and copyright header

--- ui/propertyeditor/propertyrecteditor.cpp
+++ ui/propertyeditor/propertyrecteditor.cpp
@@ -1,3 +1,31 @@
+/*
+  propertyrecteditor.cpp
+
+  This file is part of GammaRay, the Qt application inspection and
+  manipulation tool.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Hannah von Reth <hannah.vonreth@kdab.com>
+
+  Licensees holding valid commercial KDAB GammaRay licenses may use this file in
+  accordance with GammaRay Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "propertyrecteditor.h"
 #include "ui_propertyrecteditor.h"
 
@@ -5,7 +33,7 @@ using namespace GammaRay;
 
 PropertyRectEditorDialog::PropertyRectEditorDialog(const QRectF &rect, QWidget *parent)
     : QDialog(parent)
-    ,ui(new Ui::PropertyRectEditorDialog)
+    , ui(new Ui::PropertyRectEditorDialog)
 {
     ui->setupUi(this);
 
@@ -32,16 +60,16 @@ PropertyRectEditorDialog::~PropertyRectEditorDialog()
 
 QRectF PropertyRectEditorDialog::rectF() const
 {
-    if (ui->stackedWidget->currentWidget() == ui->intPage)
+    if (ui->stackedWidget->currentWidget() == ui->intPage) {
         return QRectF(ui->pointWidget->point(), ui->sizeWidget->sizeValue());
-    else
+    } else {
         return QRectF(ui->pointFWidget->pointF(), ui->sizeFWidget->sizeF());
+    }
 }
 
 PropertyRectEditor::PropertyRectEditor(QWidget *parent)
     : PropertyExtendedEditor(parent)
 {
-
 }
 
 void PropertyRectEditor::edit()
@@ -50,7 +78,6 @@ void PropertyRectEditor::edit()
     if (dlg.exec() == QDialog::Accepted)
         save(dlg.rectF().toRect());
 }
-
 
 PropertyRectFEditor::PropertyRectFEditor(QWidget *parent)
     : PropertyExtendedEditor(parent)
@@ -63,4 +90,3 @@ void PropertyRectFEditor::edit()
     if (dlg.exec() == QDialog::Accepted)
         save(dlg.rectF());
 }
-
