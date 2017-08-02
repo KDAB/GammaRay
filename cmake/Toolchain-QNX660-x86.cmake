f9ed82830465869f56b549318408cf72f62f2e0e
Kevin Funk <kevin.funk@kdab.com>
2017-08-02 13:15:51 +0200

    QNX: Work-around missing CMake compile features
    Since Qt5.7, Qt requires C++11 support. Since Qt5.7, the CMake config
    files provided by Qt also set required compile features on the Qt5 CMake
    targets.
    
    Because of a CMake bug, the QNX QCC compiler does not offer any compile
    features though, resulting in:
    
    ```
    CMake Error in
    .../gammaray-nightly-qt58-linux32-qnx66/build/CMakeFiles/CMakeTmp/CMakeLists.txt:
      No known features for CXX compiler
      "QCC"
      version 4.7.3.
    ```
    
    Upstream CMake bug report:
      https://gitlab.kitware.com/cmake/cmake/issues/17126
    
    This patch works around the issue by providing the support compiler
    features of QNX QCC by hard-coding them in CMake.

--- cmake/Toolchain-QNX660-x86.cmake
+++ cmake/Toolchain-QNX660-x86.cmake
@@ -19,4 +19,4 @@ set(QNX_TARGET_HINT "C:/qnx660/target/qnx6")
 set(CMAKE_C_FLAGS "-Vgcc_ntox86" CACHE STRING "qcc c flags" FORCE)
 set(CMAKE_CXX_FLAGS "-Vgcc_ntox86 -lang-c++" CACHE STRING "qcc cxx flags" FORCE)
 
-include(Toolchain-QNX-Common)
+include("${CMAKE_CURRENT_LIST_DIR}/Toolchain-QNX660-common.cmake")
