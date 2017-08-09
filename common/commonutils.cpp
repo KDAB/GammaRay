dab70671b61dbb5d0d257a50f74535b51f53612f
Hannah von Reth <hannah.vonreth@kdab.com>
2017-08-09 13:02:36 +0200

    Introduce CommonUtils::windowsErrorString, and check winapi calls
    GammaRay::CommonUtils::windowsErrorString prints the the message for
    GetLastError.
    
    Additionally introduce QTC_ASSERT like macros to check winapi calls.

---
+++ common/commonutils.cpp
@@ -0,0 +1,20 @@
+#include "commonutils.h"
+
+#include <windows.h>
+
+#if defined(Q_OS_WIN)
+QString GammaRay::CommonUtils::windowsErrorString()
+{
+    wchar_t *string = 0;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+                  NULL,
+                  GetLastError(),
+                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                  (LPWSTR)&string,
+                  0,
+                  NULL);
+    QString ret = QString::fromWCharArray(string);
+    LocalFree((HLOCAL)string);
+    return ret.trimmed();
+}
+#endif
