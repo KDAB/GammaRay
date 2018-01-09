1d51105aa9672b9a58e0da379a5293d1728a26e4
David Faure <faure@kde.org>
2018-01-09 09:16:38 +0100

    Jetson toolchain: also set the C++ compiler, not just the C compiler.

--- cmake/Toolchain-jetson-tk1.cmake
+++ cmake/Toolchain-jetson-tk1.cmake
@@ -33,6 +33,7 @@
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
 set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --sysroot=$ENV{SYSROOT} -mtune=cortex-a15 -march=armv7-a -mfpu=neon-vfpv4")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --sysroot=$ENV{SYSROOT} -mtune=cortex-a15 -march=armv7-a -mfpu=neon-vfpv4")
 set(CMAKE_EXE_LINKER_FLAGS "--sysroot=$ENV{SYSROOT} -Wl,-rpath-link,$ENV{SYSROOT}/usr/lib/arm-linux-gnueabihf/tegra" CACHE STRING "executable linker flags" FORCE)
