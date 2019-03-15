83ab774f59ecbd8c365f0c5662428fb5176856b1
Laurent Montel <laurent.montel@kdab.com>
2019-03-15 13:12:50 +0100

    Make it compiles against 5.13

--- core/probe.cpp
+++ core/probe.cpp
@@ -251,7 +251,7 @@ Probe::Probe(QObject *parent)
     connect(m_queueTimer, SIGNAL(timeout()),
             this, SLOT(processQueuedObjectChanges()));
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
     const auto* signal_spy_set = qt_signal_spy_callback_set.load();
 #else
     const auto* signal_spy_set = &qt_signal_spy_callback_set;
