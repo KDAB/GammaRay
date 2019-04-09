ce23342363af4d146709a53b581d0d3e1eb05d4d
Allen Winter <allen.winter@kdab.com>
2019-04-09 12:08:27 -0400

    various - update copyright year

--- cmake/FindQmlLint.cmake
+++ cmake/FindQmlLint.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2018 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# Copyright (C) 2016-2019 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 # All rights reserved.
 #
 # Author: Volker Krause <volker.krause@kdab.com>
