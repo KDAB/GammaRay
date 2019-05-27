8f3503706e45ebeaea90b4706b15a9600a2c3e66
Kevin Funk <kevin.funk@kdab.com>
2019-05-27 15:24:46 +0200

    resourcemodel: Use override

--- 3rdparty/qt/resourcemodel.h
+++ 3rdparty/qt/resourcemodel.h
@@ -68,30 +68,30 @@ public:
     ResourceModel(const QStringList &nameFilters, QDir::Filters filters,
               QDir::SortFlags sort, QObject *parent = nullptr);
     explicit ResourceModel(QObject *parent = nullptr);
-    ~ResourceModel();
+    ~ResourceModel() override;
 
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
-    QModelIndex parent(const QModelIndex &child) const;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &child) const override;
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-    QMap<int, QVariant> itemData(const QModelIndex &index) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QMap<int, QVariant> itemData(const QModelIndex &index) const override;
 
-    bool hasChildren(const QModelIndex &index = QModelIndex()) const;
-    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool hasChildren(const QModelIndex &index = QModelIndex()) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
 
-    void sort(int column, Qt::SortOrder order = Qt::AscendingOrder);
+    void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
 
-    QStringList mimeTypes() const;
-    QMimeData *mimeData(const QModelIndexList &indexes) const;
+    QStringList mimeTypes() const override;
+    QMimeData *mimeData(const QModelIndexList &indexes) const override;
     bool dropMimeData(const QMimeData *data, Qt::DropAction action,
-                      int row, int column, const QModelIndex &parent);
-    Qt::DropActions supportedDropActions() const;
+                      int row, int column, const QModelIndex &parent) override;
+    Qt::DropActions supportedDropActions() const override;
     QHash<int, QByteArray> roleNames() const override;
 
     // ResourceModel specific API
