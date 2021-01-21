ae4f62a3f568d8a1fe5b0e95a784b59631b69ea1
Daniel Nicoletti <daniel.nicoletti@kdab.com>
2021-01-21 12:21:01 -0300

    Qt6: fix signal signature warnings

--- launcher/core/injector/processinjector.cpp
+++ launcher/core/injector/processinjector.cpp
@@ -41,12 +41,18 @@ ProcessInjector::ProcessInjector()
     , mExitStatus(QProcess::NormalExit)
 {
     m_proc.setInputChannelMode(QProcess::ForwardedInputChannel);
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     connect(&m_proc, &QProcess::errorOccurred, this, &ProcessInjector::processFailed);
-    connect(&m_proc, &QProcess::finished, this, &ProcessInjector::processFinished);
 #else
     connect(&m_proc, static_cast<void(QProcess::*)(QProcess::ProcessError)>(&QProcess::error),
                this, &ProcessInjector::processFailed);
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    connect(&m_proc, &QProcess::finished, this, &ProcessInjector::processFinished);
+#elif QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+    connect(&m_proc, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),
+            this, &ProcessInjector::processFinished);
+#else
     connect(&m_proc, static_cast<void(QProcess::*)(int)>(&QProcess::finished),
             this, &ProcessInjector::processFinished);
 #endif
@@ -61,7 +67,7 @@ ProcessInjector::~ProcessInjector()
 
 void ProcessInjector::stop()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     disconnect(&m_proc, &QProcess::errorOccurred, this, &ProcessInjector::processFailed);
 #else
     disconnect(&m_proc, static_cast<void(QProcess::*)(QProcess::ProcessError)>(&QProcess::error),
