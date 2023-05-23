52b8a5feff0fc57fcc03058371b149db554239d0
Waqar Ahmed <waqar.ahmed@kdab.com>
2023-05-23 15:03:18 +0500

    Remove deprecated functions

--- plugins/guisupport/guisupport.cpp
+++ plugins/guisupport/guisupport.cpp
@@ -339,9 +339,6 @@ void GuiSupport::registerMetaTypes()
     MO_ADD_PROPERTY_O2(QBrush, color, setColor);
     MO_ADD_PROPERTY_RO(QBrush, gradient);
     MO_ADD_PROPERTY_RO(QBrush, isOpaque);
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    MO_ADD_PROPERTY(QBrush, matrix, setMatrix);
-#endif
     MO_ADD_PROPERTY(QBrush, style, setStyle);
     MO_ADD_PROPERTY(QBrush, texture, setTexture);
     MO_ADD_PROPERTY(QBrush, transform, setTransform);
@@ -357,10 +354,6 @@ void GuiSupport::registerMetaTypes()
     MO_ADD_PROPERTY(QFont, italic, setItalic);
     MO_ADD_PROPERTY(QFont, kerning, setKerning);
     MO_ADD_PROPERTY_RO(QFont, key);
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    MO_ADD_PROPERTY_RO(QFont, lastResortFamily);
-#endif
-    //     MO_ADD_PROPERTY_RO(QFont, lastResortFont); asserts at runtime!?
     MO_ADD_PROPERTY_RO(QFont, letterSpacing);
     MO_ADD_PROPERTY_RO(QFont, letterSpacingType);
     MO_ADD_PROPERTY(QFont, overline, setOverline);
