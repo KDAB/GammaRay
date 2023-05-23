c73936b5efabe3a3d8d9fb7ae1dacf81d3b644c8
Waqar Ahmed <waqar.ahmed@kdab.com>
2023-05-23 15:15:42 +0500

    Fix Qt3D::QBuffer::Type deprecation warnings
    only use it for qt below 5.10

--- plugins/qt3dinspector/geometryextension/qt3dgeometryextensioninterface.cpp
+++ plugins/qt3dinspector/geometryextension/qt3dgeometryextensioninterface.cpp
@@ -62,7 +62,7 @@ bool Qt3DGeometryAttributeData::operator==(const Qt3DGeometryAttributeData &rhs)
 QT_BEGIN_NAMESPACE
 QDataStream &operator<<(QDataStream &out, const Qt3DGeometryBufferData &data)
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
     out << data.name << data.data;
 #else
     out << data.name << data.data << data.type;
@@ -72,7 +72,7 @@ QDataStream &operator<<(QDataStream &out, const Qt3DGeometryBufferData &data)
 
 QDataStream &operator>>(QDataStream &in, Qt3DGeometryBufferData &data)
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
     in >> data.name >> data.data;
 #else
     in >> data.name >> data.data >> data.type;
