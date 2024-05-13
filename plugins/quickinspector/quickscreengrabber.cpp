d148f7dffbde7dd3adaae666726b2d947daec9f3
Waqar Ahmed <waqar.ahmed@kdab.com>
2024-05-13 12:54:24 +0500

    Fix quickwidgets with qt 6.7

--- plugins/quickinspector/quickscreengrabber.cpp
+++ plugins/quickinspector/quickscreengrabber.cpp
@@ -21,6 +21,7 @@
 #include <QEvent>
 #include <QPainter>
 #include <QQuickWindow>
+#include <QQuickRenderControl>
 
 #ifndef QT_NO_OPENGL
 #include <QOpenGLContext>
@@ -626,10 +627,21 @@ void OpenGLScreenGrabber::windowAfterRendering()
         // to find the right x,y offsets to extract and show just our quick widget
         const bool isQQuickWidget = qstrcmp(m_window->metaObject()->className(), "QQuickWidgetOffscreenWindow") == 0;
         if (isQQuickWidget) {
+#if QT_VERSION <= QT_VERSION_CHECK(6, 6, 0)
             xOff = m_renderInfo.windowPosition.x();
             const auto windowBottom = (m_renderInfo.windowPosition.y() + m_renderInfo.windowSize.height());
             const auto viewportHeight = viewport[3];
             yOff = viewportHeight - windowBottom;
+#else
+            const auto windowBottom = m_renderInfo.windowSize.height();
+            const auto rc = QQuickWindowPrivate::get(m_window)->renderControl;
+            QPoint offset;
+            rc->renderWindow(&offset);
+
+            const auto viewportHeight = viewport[3];
+            yOff = viewportHeight - windowBottom - offset.y();
+            xOff = offset.x();
+#endif
         }
 #endif
 
