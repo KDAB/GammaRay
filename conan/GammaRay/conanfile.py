bd60c840e1702c689a6eac2bb0a42f1378b1c323
Sergio Martins <sergio.martins@kdab.com>
2025-05-15 17:25:28 +0100

    bump version to 3.2.1

--- conan/GammaRay/conanfile.py
+++ conan/GammaRay/conanfile.py
@@ -12,7 +12,7 @@ from conans import ConanFile, CMake, tools
 
 class GammaRayConan(ConanFile):
     name = "GammaRay"
-    version = "3.2.0"
+    version = "3.2.1"
     license = "https://raw.githubusercontent.com/KDAB/GammaRay/{0}/LICENSE.txt".format(version)
     author = "Klaralvdalens Datakonsult AB (KDAB) info@kdab.com"
     url = "https://github.com/KDAB/GammaRay.git"
